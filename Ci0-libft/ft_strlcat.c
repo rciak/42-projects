dbd49db783a897c85782da0cbd857124872fdba9
Rene Ciak <rciakAT42Vienna@web.de>
2025-05-08 15:34:23 +0200

    Ci0-libft: Transportcommit

---
+++ Ci0-libft/ft_strlcat.c
@@ -0,0 +1,18 @@
+/**
+ * @file ft_strlcat.c
+ * @brief Stores the definition of ft_strlcat
+ */
+
+#include "libft.h"
+
+/**
+ * @brief , cf.
+ *        [ft_strlcat](https://man7.org/linux/man-pages/man3/strlcat.html).
+ * @note 
+ * @param[in] 
+ * @param[out]
+ * @param[in,out]
+ * @return 
+ *          * 
+ *          * 
+ */
