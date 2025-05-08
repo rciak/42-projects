9ee84e6f7b082dd8978b2c84c24bd6a9554a8148
Rene Angelo Ciak <reciak@c2r3p9.42vienna.com>
2025-05-08 16:34:51 +0200

    Ci0-libft: Implemented  min  and  max  functions for size_t vars

--- Ci0-libft/ft_x_math01.c
+++ Ci0-libft/ft_x_math01.c
@@ -1,18 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_x_math01.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 16:07:02 by reciak            #+#    #+#             */
+/*   Updated: 2025/05/08 16:24:18 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /**
- * @file ft_
- * @brief Stores the definition of 
+ * @file ft_x_math01.c
+ * @brief Stores some usefull math functions
  */
 
 #include "libft.h"
 
 /**
- * @brief , cf.
- *        [](https://man7.org/linux/man-pages/man3/ .html).
- * @note 
- * @param[in] 
- * @param[out]
- * @param[in,out]
- * @return 
- *          * 
- *          * 
+ * @brief Returns the minimum of two values
+ * @param[in] a The first value
+ * @param[in] b The second value
+ * @return `min(a,b)`
  */
+size_t	si_min(size_t a, size_t b)
+{
+	if (a <= b)
+		return (a);
+	return (b);
+}
+
+/**
+ * @brief Returns the maximum of two values
+ * @param[in] a The first value
+ * @param[in] b The second value
+ * @return `max(a,b)`
+ */
+ size_t	si_max(size_t a, size_t b)
+ {
+	 if (a >= b)
+		 return (a);
+	 return (b);
+ }
