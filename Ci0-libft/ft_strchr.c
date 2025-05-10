ca2abdb2e53fe41a98af4cb54dcdd75405865c9c
Rene Angelo Ciak <reciak@c2r3p9.42vienna.com>
2025-05-10 22:47:07 +0200

    Ci0-libft: Implemented ft_strchr

--- Ci0-libft/ft_strchr.c
+++ Ci0-libft/ft_strchr.c
@@ -6,25 +6,35 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 22:18:44 by reciak            #+#    #+#             */
-/*   Updated: 2025/05/10 22:19:52 by reciak           ###   ########.fr       */
+/*   Updated: 2025/05/10 22:44:32 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /**
- * @file ft_
- * @brief Stores the definition of 
+ * @file ft_strchr.c
+ * @brief Stores the definition of ft_strchr
  */
 
 #include "libft.h"
 
 /**
- * @brief , cf.
+ * @brief Finds the first occurence of a character in a string, cf.
  *        [strchr](https://man7.org/linux/man-pages/man3/strchr.3.html).
- * @note 
- * @param[in] 
- * @param[out]
- * @param[in,out]
+ * @param[in] s The string to investigate
+ * @param[in] c The character to be found
+ * @note \p c may be the null byte. In this case strchr
+ *            returns the address of it (as nullterinator of s).
  * @return 
- *          * 
- *          * 
+ *          * Address of the first occurence of \p c, if any.
+ *          * `NULL` if \p s does not contain the character \p c.
  */
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
--- Ci0-libft/libft.h
+++ Ci0-libft/libft.h
@@ -45,6 +45,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
 
 //ft_x_math01.c
 size_t	si_min(size_t a, size_t b);
