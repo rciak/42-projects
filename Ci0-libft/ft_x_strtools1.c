fd7181f305843eb92993feec7776fd85244f3b15
Rene Angelo Ciak <reciak@c1r1p8.42vienna.com>
2025-05-12 18:18:10 +0200

    Ci0-libft: Fixed bug in  is_in  by now treating a missed edge case.

--- Ci0-libft/ft_x_strtools1.c
+++ Ci0-libft/ft_x_strtools1.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 16:15:36 by reciak            #+#    #+#             */
-/*   Updated: 2025/05/12 17:00:35 by reciak           ###   ########.fr       */
+/*   Updated: 2025/05/12 18:15:30 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,26 @@
 
 /**
  * @brief Tell whether a character is in a string or not.
- * @note `is_in` is a convenient "downgrade" for ft_strchr()
+ * @note `is_in(c, str)` can in general not be replaced by the condition
+ *       `ft_strchr(str, (int) c) != NULL` as setting \p c to the
+ *       *nullterminator* illustrates:
+ * @note Consider this particular choice for `c` e.g. for `str = "0123456789"`.
+ *       Then
+ *       `is_in(c, "01234567889")` returns *false*,
+ *       whereas the condition `ft_strchr(str, (int) c) != NULL` would be
+ *       *true*, since `ft_strchr((int) c, "01234566789")`
+ *       would return the address of the 
+ *       nullterminator of "0123456789", being different from `NULL`!
  * @param[in] c A character of interest
  * @param[in] str A pointer to string that should be scanned for \p c.
  * @return 
- *          * true, if \p str contains \p c
- *          * false, if \p str does not contain \p c.
+ *          * true, if \p str contains \p c *before* the terminating  nullbyte.
+ *          * false, if \p str does not contain \p c *before*
+ *            the terminating nullbyte.
  */
 bool	is_in(char c, const char *str)
 {
+	if (c == '\0')
+		return (false);
 	return (ft_strchr(str, (int) c) != NULL);
 }
