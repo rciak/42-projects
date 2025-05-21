6eb16dea4e5c77f7c803e6f173aaeddbafe0e403
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-05-21 16:03:05 +0200

    Ci0-libft: Implemented  ft_lstdelone

---
+++ Ci0-libft/ft_lstdelone.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 15:26:18 by reciak            #+#    #+#             */
+/*   Updated: 2025/05/21 16:02:03 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file ft_lstdelone.c
+ * @brief Stores the definition of ft_lstdelone
+ */
+
+#include "libft.h"
+
+/**
+ * @brief Takes a node as parameter and frees its content
+ *        using the function \p del . Frees the node itself but
+ *        does NOT free the next node, cf. Libft Subject (Version 16.7).
+ * @note The empty list, represented by lst == NULL, is acceptable input.
+ *       In this case just nothing will happen.
+ * @warning Strangly the first argument lst is not a double pointer;
+ *          In contrast to ft_lstadd_front() where the caller's pointer
+ *          to the first node of the linked list got addopted, this function
+ *          ft_lstdelone() can not change the caller's pointer!
+ * @warning Consequently the caller need to manually adjust the pointer after
+ *          calling  ft_lstdelone().
+ * @param[in] lst: The node to free.
+ * @param[in] del: The address of the function used to delete the content.
+ */
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
