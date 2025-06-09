0ccfdde046dc5a0780c952c834da746f72081c2c
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-06-09 14:00:32 +0200

    Ci1-ft_printf: Added some Doxygen comments

--- Ci1-ft_printf/inc/ft_printf.h
+++ Ci1-ft_printf/inc/ft_printf.h
@@ -6,9 +6,14 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:48:34 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/04 19:18:28 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/09 13:56:55 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file ft_printf.h
+ * @brief The header file for reciak's ft_printf project of the Common Core
+ */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
