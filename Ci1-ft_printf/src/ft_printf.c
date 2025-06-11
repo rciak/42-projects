94e7b28cd3ca31d338b009a61cd2f922c52a6aea
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-06-11 12:56:03 +0200

    Ci1-ft_printf: Drafting of core function and some static helpers done
    Need to be implemented: out_nbr()  out_unbr()  out_vptr()

--- Ci1-ft_printf/src/ft_printf.c
+++ Ci1-ft_printf/src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:40:46 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/09 20:40:01 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/11 12:54:55 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 #include "ft_printf.h"
 
-static int	st_write_by_spec(const char *str);
+static int	st_write_by_specifi(const char *str, va_list *parg);
 
 /**
  * @brief Mimicks the orignal ft_printf, but only for the following conversion
@@ -50,57 +50,80 @@ static int	st_write_by_spec(const char *str);
    undefined.
    @endverbatim
  */
-
-//TODO: INCORPORATE va_ stuff...
 int	ft_printf(const char *str, ...)
 {
-	const char	*specifiers = "cspdiuxX%";
+	va_list		arg;
 	int			bytes_sent;
 	int			bytes_total;
 
-	bytes_sent = 0;
+	va_start (arg, str);
 	bytes_total = 0;
 	while (*str)
 	{
 		if (*str != '%')
 			bytes_sent = write(STDOUT_FD, str, 1);
 		else
-			bytes_sent = st_write_by_spec(str, );
+			bytes_sent = st_write_by_specifi(str, &arg);
 		if (bytes_sent < 0)
+		{
+			va_end(arg);
 			return (bytes_sent);
+		}
 		bytes_total += bytes_sent;
 		str++;
 	}
+	va_end(arg);
 	return (bytes_total);
 }
 
 /**
-   @note the caller relies on st_write_by_spec() taking care itself 
-        of the edge case of the current '%' (== *str) being followed by '\0'.
+ * @note the caller relies on st_write_by_specifi() taking care itself
+ *      of the edge case of the current '%' (== *str) being followed by '\0'.
+ * @note va_arg returns only "fully promoted" types, like e.g. int or double.
+ *       For the case `*str == 'c'`, thus a casting from int to (unsigned char)
+ *       is done iside the called static helper function;
+ * @note Note here that casting to (unsigned char) is prefered over casting
+ *       to (char) since this seems to be what printf is doing, cf.
+ *       the man page of printf (`man 3 printf`), sect.
+ *       "Conversion specifiers", c. (Although the raw byte / bit pattern
+ *       resulting from either casting to (unsigned char) or (signed char)
+ *       is the same, so `write` will treat them in the same way.
+ * @warning The result of `va_arg` is *not an lvalue*; in contrast to
+ *          lvalues which have a valid address for their life time,
+ *          the result of va_arg is just a value and might have only a
+ *          very limited "temporary lifetime" (as perplexity put it nicely.)
  */
-static int	st_write_by_spec(const char *str)
+static int	st_write_by_specifi(const char *s, va_list *parg)
 {
-	if (!(*str == '%'))
+	if (*s != '%')
 		return (E_NOT_AT_PERCENT);
-	str++;
-	if (! *str)
+	s++;
+	if (! *s)
 		return (0);
-	if (*str == 'c')
-		return (put_c(str));
-	else if (*str == 's')
-		return (put_s(str));
-	else if (*str == 'p')
-		return (put_p(str));
-	else if (*str == 'd' && *str == 'i')
-		return (put_d(str));
-	else if (*str == 'u')
-		return (put_u(str));
-	else if (*str == 'x')
-		return (put_x(str));
-	else if (*str == 'X')
-		return (put_captial_x(str));
-	else if (*str == '%')
-		return (write(STDOUT_FD, str, 1));
+	if (is_in(*s, "di"))
+		return (out_nbr(va_arg(*parg, int), "0123456789"));
+	else if (*s == 'u')
+		return (out_unbr(va_arg(*parg, unsigned int), "0123456789"));
+	else if (*s == 'x')
+		return (out_unbr(va_arg(*parg, unsigned int), "0123456789abcdef"));
+	else if (*s == 'X')
+		return (out_unbr(va_arg(*parg, unsigned int), "0123456789ABCDEF"));
+	else if (*s == 'p')
+		return (out_vptr(va_arg(*parg, void *)));
+	else if (*s == 's')
+		return (ft_putstr(va_arg(*parg, char *)));
+	else if (*s == 'c')
+		return (st_out_c(va_arg(*parg, int)));
+	else if (*s == '%')
+		return (write(STDOUT_FD, s, 1));
 	else
 		return (E_BAD_ARG);
+}
+
+static int	t_out_c(va_list *parg)
+{
+	unsigned char	uc;
+
+	uc = (unsigned char) va_arg(*parg, int);
+	return (write(STDOUT_FD, &uc, 1));
 }
