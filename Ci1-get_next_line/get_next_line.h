532088485ea25021c7a77779635002940becc711
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-06-27 17:14:07 +0200

    Ci1-gnl: Setup basic header content for get_next_line.h

--- Ci1-get_next_line/get_next_line.h
+++ Ci1-get_next_line/get_next_line.h
@@ -6,7 +6,15 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 15:54:45 by reciak            #+#    #+#             */
-/*   Updated: 2025/06/27 15:54:56 by reciak           ###   ########.fr       */
+/*   Updated: 2025/06/27 17:13:25 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>    //malloc, free
+# include <unistd.h>    //read
+
+char	*get_next_line(int fd);
+
+#endif
