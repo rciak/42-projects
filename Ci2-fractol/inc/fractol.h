620041037b6ff79df129c987968cf871e1e9fc91
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2025-07-12 21:35:58 +0200

    Ci2-fractol: STEP 1: Secure Copy. mlx stuff unprotected but seems working
    Currently only reacting on mousebuttons as proof of concept ->
    Currently closing the window gives positive valgrind result

--- Ci2-fractol/inc/fractol.h
+++ Ci2-fractol/inc/fractol.h
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 15:41:40 by reciak            #+#    #+#             */
-/*   Updated: 2025/07/12 16:39:53 by reciak           ###   ########.fr       */
+/*   Updated: 2025/07/12 21:01:44 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
  */
 #ifndef FRACTOL_H
 # define FRACTOL_H
+# include <X11/X.h>
+# include <X11/keysym.h>
+//
+// Wenn das so nicht funktioniert:
+// # define XK_MISCELLANY
+// # define XK_LATIN1
+// # include <X11/keysymdef.h>
+//
 # include <mlx.h>
 # include "libft.h"
 
@@ -31,6 +39,11 @@ enum e_geo_win
 {
 	WIN_WIDTH = 1025,
 	WIN_HEIGHT = 1025
+};
+
+enum e_misc
+{
+	MLX_WILL_ANYWAY_TROUGH_AWAY_THE_RETURN_VAL = 314
 };
 
 #endif
--- Ci2-fractol/src/main.c
+++ Ci2-fractol/src/main.c
@@ -6,27 +6,71 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 11:25:06 by reciak            #+#    #+#             */
-/*   Updated: 2025/07/12 16:43:43 by reciak           ###   ########.fr       */
+/*   Updated: 2025/07/12 21:22:25 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-#include <mlx.h>
 
-int main (void)
+typedef struct s_all
 {
 	void	*x_serv;
 	void	*win;
+	char	*dummy_c;
+	int		dummy_i;
+} t_all;
 
-	x_serv = mlx_init();
+int draw_fractal(t_all all);
+int react_on_mouse(int button, int k, int j, t_all *all);
+
+
+int main (void)
+{
+	t_all all;
+
+	all.dummy_c = "Message";
+	all.dummy_i = 3;
+
+	all.x_serv = mlx_init();
 	//TODO NULL CHECK
-	win = mlx_new_window(x_serv, WIN_WIDTH, WIN_HEIGHT, "Fractol");
-	mlx_destroy_window(x_serv, win);
-	//free (win);
-	mlx_destroy_display(x_serv);
-	free(x_serv);
+	all.win = mlx_new_window(all.x_serv, WIN_WIDTH, WIN_HEIGHT, "Fractol");
+	//TODO NULL CHECK
 
+	mlx_loop_hook(all.x_serv, &draw_fractal, &all);
+	mlx_mouse_hook(all.win, &react_on_mouse, &all);
+	mlx_loop(all.x_serv);
 
-	ft_putstr_fd("Hello World!", 1);
+	mlx_destroy_display(all.x_serv);
+	free(all.x_serv);
+	
+	return (0);
 }
 
+int draw_fractal(t_all all)
+{
+	if (all.dummy_i == 3)
+	{
+		ft_putstr_fd("Set i to 0\n", 1);
+		all.dummy_i = 0;
+	}
+	return (MLX_WILL_ANYWAY_TROUGH_AWAY_THE_RETURN_VAL);
+}
+
+int react_on_mouse(int button, int k, int l, t_all *all)
+{
+	if (k < l)
+		k = l;
+	if (button == Button1)
+	{
+		ft_putstr_fd("\nButton1! ", 1);
+		ft_putstr_fd(all->dummy_c, 1);
+		all->dummy_i++;
+	}
+	if (button == Button2)
+	{
+		ft_putstr_fd("\nButton2! ", 1);
+	}
+	if (button == Button3)
+		mlx_destroy_window(all->x_serv, all->win);
+	return (0);
+}
