e301f5dae6200c938cb98d0aa18b638c4bb66184
Rene Angelo Ciak <reciak@c3r2p2.42vienna.com>
2025-07-28 16:00:10 +0200

    Ci2-push_swap: Implemented group tool: group_already_sorted

---
+++ Ci2-push_swap/src/group_tools/group_already_sorted.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   group_already_sorted.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 11:45:49 by reciak            #+#    #+#             */
+/*   Updated: 2025/07/28 12:05:28 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file group_already_sorted.c 
+ * @brief Stores the definition of group_already_sorted()
+ */
+
+#include "libft.h"
+#include "push_swap.h"
+
+/**
+ * @brief Checks if a group of nodes insides a doubly linked list is already
+ *        sorted.
+ * @note This function is used by main to check if there is need to sort at all.
+ * @param[in] node Any node of a group
+ * @return 
+ *          * `true` if the group is already sorted
+ *          * `false` else
+ */
+bool	group_already_sorted(t_dl_node *node)
+{
+	int	size;
+
+	while (((t_ps_obj*)node->obj)->group.starts == false)
+		node = node->prev;
+	size = group_size(node);
+	while (1 < size)
+	{
+		if (((t_ps_obj*)node->obj)->n >= ((t_ps_obj*)node->next->obj)->n)
+			return (false);
+		node=node->next;
+		size--;
+	}
+	return (true);
+}
