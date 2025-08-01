564836434611b0b6413fcd18f479699452eb829f
Rene Angelo Ciak <reciak@c1r7p3.42vienna.com>
2025-08-01 18:22:14 +0200

    Ci2-push_swap|draft: Kicked out variable from helper that they will not need

--- Ci2-push_swap/src/join_green_directsort/join_green_size_1.c
+++ Ci2-push_swap/src/join_green_directsort/join_green_size_1.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 17:37:50 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/01 18:11:33 by reciak           ###   ########.fr       */
+/*   Updated: 2025/08/01 18:20:45 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 
 #include "push_swap.h"
 
-static void	sortin__a_top(t_dl_node *group, t_dl_node **stack);
-static void	sortin__a_end(t_dl_node *group, t_dl_node **stack);
-static void	sortin__b_top(t_dl_node *group, t_dl_node **stack);
-static void	sortin__b_end(t_dl_node *group, t_dl_node **stack);
+static void	sortin__a_top(t_dl_node **stack);
+static void	sortin__a_end(t_dl_node **stack);
+static void	sortin__b_top(t_dl_node **stack);
+static void	sortin__b_end(t_dl_node **stack);
 
 /**
  * @brief For putting size 1 groups on top of the green area on stack `a`
@@ -31,33 +31,33 @@ void	join_green_size_1(t_dl_node *group, t_dl_node **stack)
 
 	last = group_memb_last(group);
 	if (group == stack[A])
-		sortin__a_top(group, stack);
+		sortin__a_top(stack);
 	else if (last == stack[A]->prev)
-		sortin__a_end(group, stack);
+		sortin__a_end(stack);
 	else if (group == stack[B])
-		sortin__b_top(group, stack);
+		sortin__b_top(stack);
 	else if (last == stack[B]->prev)
-		sortin__b_end(group, stack);
+		sortin__b_end(stack);
 	else
 		h_err_exit(error(ERR_LOGIC), "join_green_size_1");
 }
 
-static void	sortin__a_top(t_dl_node *group, t_dl_node **stack)
+static void	sortin__a_top(t_dl_node **stack)
 {
 
 }
 
-static void	sortin__a_end(t_dl_node *group, t_dl_node **stack)
+static void	sortin__a_end(t_dl_node **stack)
 {
 
 }
 
-static void	sortin__b_top(t_dl_node *group, t_dl_node **stack)
+static void	sortin__b_top(t_dl_node **stack)
 {
 
 }
 
-static void	sortin__b_end(t_dl_node *group, t_dl_node **stack)
+static void	sortin__b_end(t_dl_node **stack)
 {
 
 }
--- Ci2-push_swap/src/join_green_directsort/join_green_size_2.c
+++ Ci2-push_swap/src/join_green_directsort/join_green_size_2.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 18:08:44 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/01 18:15:17 by reciak           ###   ########.fr       */
+/*   Updated: 2025/08/01 18:20:21 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 
 #include "push_swap.h"
 
-static void	sortin__a_top(t_dl_node *group, t_dl_node **stack);
-static void	sortin__a_end(t_dl_node *group, t_dl_node **stack);
-static void	sortin__b_top(t_dl_node *group, t_dl_node **stack);
-static void	sortin__b_end(t_dl_node *group, t_dl_node **stack);
+static void	sortin__a_top(t_dl_node **stack);
+static void	sortin__a_end(t_dl_node **stack);
+static void	sortin__b_top(t_dl_node **stack);
+static void	sortin__b_end(t_dl_node **stack);
 
 /**
  * @brief For putting size 1 groups on top of the green area on stack `a`
@@ -31,33 +31,33 @@ void	join_green_size_2(t_dl_node *group, t_dl_node **stack)
 
 	last = group_memb_last(group);
 	if (group == stack[A])
-		sortin__a_top(group, stack);
+		sortin__a_top(stack);
 	else if (last == stack[A]->prev)
-		sortin__a_end(group, stack);
+		sortin__a_end(stack);
 	else if (group == stack[B])
-		sortin__b_top(group, stack);
+		sortin__b_top(stack);
 	else if (last == stack[B]->prev)
-		sortin__b_end(group, stack);
+		sortin__b_end(stack);
 	else
 		h_err_exit(error(ERR_LOGIC), "join_green_size_2");
 }
 
-static void	sortin__a_top(t_dl_node *group, t_dl_node **stack)
+static void	sortin__a_top(t_dl_node **stack)
 {
 
 }
 
-static void	sortin__a_end(t_dl_node *group, t_dl_node **stack)
+static void	sortin__a_end(t_dl_node **stack)
 {
 
 }
 
-static void	sortin__b_top(t_dl_node *group, t_dl_node **stack)
+static void	sortin__b_top(t_dl_node **stack)
 {
 
 }
 
-static void	sortin__b_end(t_dl_node *group, t_dl_node **stack)
+static void	sortin__b_end(t_dl_node **stack)
 {
 
 }
