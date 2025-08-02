4977a17b044c2a8af5ccb694c32f29e99f40d137
Rene Angelo Ciak <reciak@c1r7p3.42vienna.com>
2025-08-02 22:52:12 +0200

    Ci2-push_swap|feat: Implemented r_it

--- Ci2-push_swap/src/operations_generalized/r_it.c
+++ Ci2-push_swap/src/operations_generalized/r_it.c
@@ -6,27 +6,29 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 10:04:13 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/02 10:04:15 by reciak           ###   ########.fr       */
+/*   Updated: 2025/08/02 22:48:40 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /**
- * @file 
- * @brief Stores the definition of ()
+ * @file r_it
+ * @brief Stores the definition of r_it()
  */
 
 #include "push_swap.h"
 
 /**
- * @brief 
- *
+ * @brief Rotates a given node, being on either stack `a` stack `b` by
+ *        calling ra or rb.
  * @param[in, out] stack stack[A] and stack[B], respectively, are pointers to 
  *                       the first node of stack `a` and stack `b`,
  *                       respectively, if any; an empty stack is indicated by
  *                       `NULL`.
  */
-void	(t_dl_node *node, t_dl_node **stack);
-void	(t_dl_node *node, **stack)
+void	r_it(t_dl_node *node, **stack)
 {
-	
+	if (is_on_a(node))
+		ra(stack);
+	else
+	 	rb(stack);
 }
