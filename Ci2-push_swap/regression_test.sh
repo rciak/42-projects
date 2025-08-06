cd5edb4c78d4e3496ed24cd29257b7f365ba1e92
Rene Angelo Ciak <reciak@c1r7p3.42vienna.com>
2025-08-06 11:55:24 +0200

    Ci2-push_swap|tool: Added regression_test.sh for debugging process

---
+++ Ci2-push_swap/regression_test.sh
@@ -0,0 +1,185 @@
+#! /bin/bash
+
+echo "************"
+echo "Preparing..."
+echo "************"
+
+# WITH AI Support:
+cd ~/github/42-projects || exit 1
+mkdir -p TEST
+rsync -av --exclude='42-eva-lua' Ci2-push_swap TEST/Ci2-push_swap
+find Ci2-push_swap/src -type f -name "*.c" \
+    -not -path "Ci2-push_swap/src/operations_basic/*" | while read -r file; do
+        sed -i'' '/ft_put/d' "$file"
+done
+cd Ci2-push_swap || exit 1
+
+# END of PERPLEXITY supported SCRIPT
+echo
+echo "***************************************************"
+echo "Building manual-test and renaming it to ./push_swap"
+echo "***************************************************"
+make DEBUG_VALGRIND=1 manual-test; cp 42-eva-lua/Ci2-push_swap-crtests/manual_test_push_swap .; mv manual_test_push_swap push_swap > /dev/null
+
+
+./push_swap 1   |./checker_linux             1 
+./push_swap 1 2     |./checker_linux           1 2 
+./push_swap 2 1     |./checker_linux           2 1 
+./push_swap 1 2 3       |./checker_linux         1 2 3 
+./push_swap 2 3 1       |./checker_linux         2 3 1 
+./push_swap 3 1 2       |./checker_linux         3 1 2 
+./push_swap 3 2 1       |./checker_linux         3 2 1 
+./push_swap 2 1 3       |./checker_linux         2 1 3 
+./push_swap 1 3 2       |./checker_linux         1 3 2 
+./push_swap 1 2 3 4         |./checker_linux       1 2 3 4 
+./push_swap 1 2 4 3         |./checker_linux       1 2 4 3 
+./push_swap 1 3 2 4         |./checker_linux       1 3 2 4 
+./push_swap 1 3 4 2         |./checker_linux       1 3 4 2 
+./push_swap 1 4 2 3         |./checker_linux       1 4 2 3 
+./push_swap 1 4 3 2         |./checker_linux       1 4 3 2 
+./push_swap 2 1 3 4         |./checker_linux       2 1 3 4 
+./push_swap 2 1 4 3         |./checker_linux       2 1 4 3 
+./push_swap 2 3 1 4         |./checker_linux       2 3 1 4 
+./push_swap 2 3 4 1         |./checker_linux       2 3 4 1 
+./push_swap 2 4 1 3         |./checker_linux       2 4 1 3 
+./push_swap 2 4 3 1         |./checker_linux       2 4 3 1 
+./push_swap 3 1 2 4         |./checker_linux       3 1 2 4 
+./push_swap 3 1 4 2         |./checker_linux       3 1 4 2 
+./push_swap 3 2 1 4         |./checker_linux       3 2 1 4 
+./push_swap 3 2 4 1         |./checker_linux       3 2 4 1 
+./push_swap 3 4 1 2         |./checker_linux       3 4 1 2 
+./push_swap 3 4 2 1         |./checker_linux       3 4 2 1 
+./push_swap 4 1 2 3         |./checker_linux       4 1 2 3 
+./push_swap 4 1 3 2         |./checker_linux       4 1 3 2 
+./push_swap 4 2 1 3         |./checker_linux       4 2 1 3 
+./push_swap 4 2 3 1         |./checker_linux       4 2 3 1 
+./push_swap 4 3 1 2         |./checker_linux       4 3 1 2 
+./push_swap 4 3 2 1         |./checker_linux       4 3 2 1 
+./push_swap 1 2 3 4 5           |./checker_linux     1 2 3 4 5 
+./push_swap 1 2 3 5 4           |./checker_linux     1 2 3 5 4 
+./push_swap 1 2 4 3 5           |./checker_linux     1 2 4 3 5 
+./push_swap 1 2 4 5 3           |./checker_linux     1 2 4 5 3 
+./push_swap 1 2 5 3 4           |./checker_linux     1 2 5 3 4 
+./push_swap 1 2 5 4 3           |./checker_linux     1 2 5 4 3 
+./push_swap 1 3 2 4 5           |./checker_linux     1 3 2 4 5 
+./push_swap 1 3 2 5 4           |./checker_linux     1 3 2 5 4 
+./push_swap 1 3 4 2 5           |./checker_linux     1 3 4 2 5 
+./push_swap 1 3 4 5 2           |./checker_linux     1 3 4 5 2 
+./push_swap 1 3 5 2 4           |./checker_linux     1 3 5 2 4 
+./push_swap 1 3 5 4 2           |./checker_linux     1 3 5 4 2 
+./push_swap 1 4 2 3 5           |./checker_linux     1 4 2 3 5 
+./push_swap 1 4 2 5 3           |./checker_linux     1 4 2 5 3 
+./push_swap 1 4 3 2 5           |./checker_linux     1 4 3 2 5 
+./push_swap 1 4 3 5 2           |./checker_linux     1 4 3 5 2 
+./push_swap 1 4 5 2 3           |./checker_linux     1 4 5 2 3 
+./push_swap 1 4 5 3 2           |./checker_linux     1 4 5 3 2 
+./push_swap 1 5 2 3 4           |./checker_linux     1 5 2 3 4 
+./push_swap 1 5 2 4 3           |./checker_linux     1 5 2 4 3 
+./push_swap 1 5 3 2 4           |./checker_linux     1 5 3 2 4 
+./push_swap 1 5 3 4 2           |./checker_linux     1 5 3 4 2 
+./push_swap 1 5 4 2 3           |./checker_linux     1 5 4 2 3 
+./push_swap 1 5 4 3 2           |./checker_linux     1 5 4 3 2 
+./push_swap 2 1 3 4 5           |./checker_linux     2 1 3 4 5 
+./push_swap 2 1 3 5 4           |./checker_linux     2 1 3 5 4 
+./push_swap 2 1 4 3 5           |./checker_linux     2 1 4 3 5 
+./push_swap 2 1 4 5 3           |./checker_linux     2 1 4 5 3 
+./push_swap 2 1 5 3 4           |./checker_linux     2 1 5 3 4 
+./push_swap 2 1 5 4 3           |./checker_linux     2 1 5 4 3 
+./push_swap 2 3 1 4 5           |./checker_linux     2 3 1 4 5 
+./push_swap 2 3 1 5 4           |./checker_linux     2 3 1 5 4 
+./push_swap 2 3 4 1 5           |./checker_linux     2 3 4 1 5 
+./push_swap 2 3 4 5 1           |./checker_linux     2 3 4 5 1 
+./push_swap 2 3 5 1 4           |./checker_linux     2 3 5 1 4 
+./push_swap 2 3 5 4 1           |./checker_linux     2 3 5 4 1 
+./push_swap 2 4 1 3 5           |./checker_linux     2 4 1 3 5 
+./push_swap 2 4 1 5 3           |./checker_linux     2 4 1 5 3 
+./push_swap 2 4 3 1 5           |./checker_linux     2 4 3 1 5 
+./push_swap 2 4 3 5 1           |./checker_linux     2 4 3 5 1 
+./push_swap 2 4 5 1 3           |./checker_linux     2 4 5 1 3 
+./push_swap 2 4 5 3 1           |./checker_linux     2 4 5 3 1 
+./push_swap 2 5 1 3 4           |./checker_linux     2 5 1 3 4 
+./push_swap 2 5 1 4 3           |./checker_linux     2 5 1 4 3 
+./push_swap 2 5 3 1 4           |./checker_linux     2 5 3 1 4 
+./push_swap 2 5 3 4 1           |./checker_linux     2 5 3 4 1 
+./push_swap 2 5 4 1 3           |./checker_linux     2 5 4 1 3 
+./push_swap 2 5 4 3 1           |./checker_linux     2 5 4 3 1 
+./push_swap 3 1 2 4 5           |./checker_linux     3 1 2 4 5 
+./push_swap 3 1 2 5 4           |./checker_linux     3 1 2 5 4 
+./push_swap 3 1 4 2 5           |./checker_linux     3 1 4 2 5 
+./push_swap 3 1 4 5 2           |./checker_linux     3 1 4 5 2 
+./push_swap 3 1 5 2 4           |./checker_linux     3 1 5 2 4 
+./push_swap 3 1 5 4 2           |./checker_linux     3 1 5 4 2 
+./push_swap 3 2 1 4 5           |./checker_linux     3 2 1 4 5 
+./push_swap 3 2 1 5 4           |./checker_linux     3 2 1 5 4 
+./push_swap 3 2 4 1 5           |./checker_linux     3 2 4 1 5 
+./push_swap 3 2 4 5 1           |./checker_linux     3 2 4 5 1 
+./push_swap 3 2 5 1 4           |./checker_linux     3 2 5 1 4 
+./push_swap 3 2 5 4 1           |./checker_linux     3 2 5 4 1 
+./push_swap 3 4 1 2 5           |./checker_linux     3 4 1 2 5 
+./push_swap 3 4 1 5 2           |./checker_linux     3 4 1 5 2 
+./push_swap 3 4 2 1 5           |./checker_linux     3 4 2 1 5 
+./push_swap 3 4 2 5 1           |./checker_linux     3 4 2 5 1 
+./push_swap 3 4 5 1 2           |./checker_linux     3 4 5 1 2 
+./push_swap 3 4 5 2 1           |./checker_linux     3 4 5 2 1 
+./push_swap 3 5 1 2 4           |./checker_linux     3 5 1 2 4 
+./push_swap 3 5 1 4 2           |./checker_linux     3 5 1 4 2 
+./push_swap 3 5 2 1 4           |./checker_linux     3 5 2 1 4 
+./push_swap 3 5 2 4 1           |./checker_linux     3 5 2 4 1 
+./push_swap 3 5 4 1 2           |./checker_linux     3 5 4 1 2 
+./push_swap 3 5 4 2 1           |./checker_linux     3 5 4 2 1 
+./push_swap 4 1 2 3 5           |./checker_linux     4 1 2 3 5 
+./push_swap 4 1 2 5 3           |./checker_linux     4 1 2 5 3 
+./push_swap 4 1 3 2 5           |./checker_linux     4 1 3 2 5 
+./push_swap 4 1 3 5 2           |./checker_linux     4 1 3 5 2 
+./push_swap 4 1 5 2 3           |./checker_linux     4 1 5 2 3 
+./push_swap 4 1 5 3 2           |./checker_linux     4 1 5 3 2 
+./push_swap 4 2 1 3 5           |./checker_linux     4 2 1 3 5 
+./push_swap 4 2 1 5 3           |./checker_linux     4 2 1 5 3 
+./push_swap 4 2 3 1 5           |./checker_linux     4 2 3 1 5 
+./push_swap 4 2 3 5 1           |./checker_linux     4 2 3 5 1 
+./push_swap 4 2 5 1 3           |./checker_linux     4 2 5 1 3 
+./push_swap 4 2 5 3 1           |./checker_linux     4 2 5 3 1 
+./push_swap 4 3 1 2 5           |./checker_linux     4 3 1 2 5 
+./push_swap 4 3 1 5 2           |./checker_linux     4 3 1 5 2 
+./push_swap 4 3 2 1 5           |./checker_linux     4 3 2 1 5 
+./push_swap 4 3 2 5 1           |./checker_linux     4 3 2 5 1 
+./push_swap 4 3 5 1 2           |./checker_linux     4 3 5 1 2 
+./push_swap 4 3 5 2 1           |./checker_linux     4 3 5 2 1 
+./push_swap 4 5 1 2 3           |./checker_linux     4 5 1 2 3 
+./push_swap 4 5 1 3 2           |./checker_linux     4 5 1 3 2 
+./push_swap 4 5 2 1 3           |./checker_linux     4 5 2 1 3 
+./push_swap 4 5 2 3 1           |./checker_linux     4 5 2 3 1 
+./push_swap 4 5 3 1 2           |./checker_linux     4 5 3 1 2 
+./push_swap 4 5 3 2 1           |./checker_linux     4 5 3 2 1 
+./push_swap 5 1 2 3 4           |./checker_linux     5 1 2 3 4 
+./push_swap 5 1 2 4 3           |./checker_linux     5 1 2 4 3 
+./push_swap 5 1 3 2 4           |./checker_linux     5 1 3 2 4 
+./push_swap 5 1 3 4 2           |./checker_linux     5 1 3 4 2 
+./push_swap 5 1 4 2 3           |./checker_linux     5 1 4 2 3 
+./push_swap 5 1 4 3 2           |./checker_linux     5 1 4 3 2 
+./push_swap 5 2 1 3 4           |./checker_linux     5 2 1 3 4 
+./push_swap 5 2 1 4 3           |./checker_linux     5 2 1 4 3 
+./push_swap 5 2 3 1 4           |./checker_linux     5 2 3 1 4 
+./push_swap 5 2 3 4 1           |./checker_linux     5 2 3 4 1 
+./push_swap 5 2 4 1 3           |./checker_linux     5 2 4 1 3 
+./push_swap 5 2 4 3 1           |./checker_linux     5 2 4 3 1 
+./push_swap 5 3 1 2 4           |./checker_linux     5 3 1 2 4 
+./push_swap 5 3 1 4 2           |./checker_linux     5 3 1 4 2 
+./push_swap 5 3 2 1 4           |./checker_linux     5 3 2 1 4 
+./push_swap 5 3 2 4 1           |./checker_linux     5 3 2 4 1 
+./push_swap 5 3 4 1 2           |./checker_linux     5 3 4 1 2 
+./push_swap 5 3 4 2 1           |./checker_linux     5 3 4 2 1 
+./push_swap 5 4 1 2 3           |./checker_linux     5 4 1 2 3 
+./push_swap 5 4 1 3 2           |./checker_linux     5 4 1 3 2 
+./push_swap 5 4 2 1 3           |./checker_linux     5 4 2 1 3 
+./push_swap 5 4 2 3 1           |./checker_linux     5 4 2 3 1 
+./push_swap 5 4 3 1 2           |./checker_linux     5 4 3 1 2 
+./push_swap 5 4 3 2 1           |./checker_linux     5 4 3 2 1
+
+
+bash -c 'ARG="1 2 3 4 7 6 5"; ./push_swap $ARG | ./checker_linux $ARG'
+
+echo "------------------------------------------------------------------------"
+echo "regression_test.sh: All test executed                                   "
+echo "They passed iff there is no error output like 'KO' above the dashed line"
+
