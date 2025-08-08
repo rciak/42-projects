4895a0d56a9e1a37ee1bf756d7fd7496e12e7c48
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2025-08-08 18:20:59 +0200

    Ci2-push_swap|feat: !Still logical error instide! implemened join_green_size_3
    Although it has a logical bug (a group on stack b is ranked inverse to the
    same group an a) it is still commited since it is a very good starting point
    to fixing the bug; bug is visible for the now failing test case
    
    make DEBUG_VALGRIND=1 manual-test; cp 42-eva-lua/Ci2-push_swap-crtests/manual_test_push_swap .; mv manual_test_push_swap push_swap
    bash -c 'ARG="58 691 -519 6 320 -179 -997 -868 -353 1136 -984 -68 303 -922 785 -726 -330 1175 -61 337"; ./push_swap $ARG'

--- Ci2-push_swap/src/join_green_directsort/join_green_size_3.c
+++ Ci2-push_swap/src/join_green_directsort/join_green_size_3.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 12:14:11 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/07 17:43:04 by reciak           ###   ########.fr       */
+/*   Updated: 2025/08/08 18:13:59 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,49 +19,49 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-static void	sortin__a_top(t_dl_node **stack, int *rank, size_t m, bool cyclic);
-static void	sortin__a_end(t_dl_node **stack, int *rank, size_t m, bool cyclic);
-static void	sortin__b_top(t_dl_node **stack, int *rank, size_t m, bool cyclic);
-static void	sortin__b_end(t_dl_node **stack, int *rank, size_t m, bool cyclic);
+static void	sortin__a_top(t_dl_node **stack, size_t i_max, bool cyclic);
+static void	sortin__a_end(t_dl_node **stack, size_t i_max, bool cyclic);
+static void	sortin__b_top(t_dl_node **stack, size_t i_max, bool cyclic);
+static void	sortin__b_end(t_dl_node **stack, size_t i_max, bool cyclic);
 
 /**
  * @brief For putting size 3 groups on top of the green area on stack `a`
+ * @note regarding the variable \p i_max_min note that
  */
 void	join_green_size_3(t_dl_node *group, t_dl_node **stack)
 {
-	int			rank[3];
-	size_t		i_max;
+	size_t		i_max_min;
 	bool		cyclic;
 	t_dl_node	*last;
 
-	rank[0] = ((t_ps_obj *) group->obj)->group.rank;
-	rank[1] = ((t_ps_obj *) group->next->obj)->group.rank;
-	rank[2] = ((t_ps_obj *) group->next->next->obj)->group.rank;
-	i_max = 0;
-	while (rank[i_max] != 3)
-		i_max++;
+	if (((t_ps_obj *) group->obj)->group.rank == 3)
+		i_max_min = 0;
+	else if (((t_ps_obj *) group->next->obj)->group.rank == 3)
+		i_max_min = 1;
+	else if (((t_ps_obj *) group->next->next->obj)->group.rank == 3)
+		i_max_min = 2;
 	cyclic = is_cyclic(group);
 	last = group_memb_last(group);
 	if (group == stack[A])
-		sortin__a_top(stack, rank, i_max, cyclic);
+		sortin__a_top(stack, i_max_min, cyclic);
 	else if (last == stack[A]->prev)
-		sortin__a_end(stack);
+		sortin__a_end(stack, i_max_min, cyclic);
 	else if (group == stack[B])
-		sortin__b_top(stack);
+		sortin__b_top(stack, i_max_min, cyclic);
 	else if (last == stack[B]->prev)
-		sortin__b_end(stack);
+		sortin__b_end(stack, i_max_min, cyclic);
 	else
 		h_err_exit(error(ERR_LOGIC), "join_green_size_3");
 }
 
-static void	sortin__a_top(t_dl_node **stack, int *rank, size_t m, bool cyclic)
+static void	sortin__a_top(t_dl_node **stack, size_t i_max, bool cyclic)
 {
-	if (m == 2)
+	if (i_max == 2)
 	{
 		if (!cyclic)
 			sa(stack);
 	}
-	else if (m == 1)
+	else if (i_max == 1)
 	{
 		ra(stack);
 		sa(stack);
@@ -69,7 +69,7 @@ static void	sortin__a_top(t_dl_node **stack, int *rank, size_t m, bool cyclic)
 		if (cyclic)
 			sa(stack);
 	}
-	else if (m == 0)
+	else if (i_max == 0)
 	{
 		sa(stack);
 		ra(stack);
@@ -80,31 +80,95 @@ static void	sortin__a_top(t_dl_node **stack, int *rank, size_t m, bool cyclic)
 	}
 }
 
-static void	sortin__a_end(t_dl_node **stack, int *rank, size_t m, bool cyclic)
+static void	sortin__a_end(t_dl_node **stack, size_t i_max, bool cyclic)
 {
-	// rra(stack);
-	// rra(stack);
-	// if (((t_ps_obj *) stack[A]->obj)->n
-	// 	> ((t_ps_obj *) stack[A]->next->obj)->n)
-	// 	sa(stack);
+	rra(stack);
+	rra(stack);
+	if (i_max == 2)
+	{
+		rra(stack);
+		if (!cyclic)
+			sa(stack);
+	}
+	else if (i_max == 1)
+	{
+		sa(stack);
+		rra(stack);
+		if (cyclic)
+			sa(stack);
+	}
+	else if (i_max == 0)
+	{
+		pb(stack);
+		rra(stack);
+		sa(stack);
+		pa(stack);
+		if (!cyclic)
+			sa(stack);
+	}
 }
 
-static void	sortin__b_top(t_dl_node **stack, int *rank, size_t m, bool cyclic)
+static void	sortin__b_top(t_dl_node **stack, size_t i_max, bool cyclic)
 {
-	// pa(stack);
-	// pa(stack);
-	// if (((t_ps_obj *) stack[A]->obj)->n
-	// 	> ((t_ps_obj *) stack[A]->next->obj)->n)
-	// 	sa(stack);
+static int j = 0; j++; dl_lst_linearize(stack[A]); dl_lst_linearize(stack[B]); print_stacks(stack); dl_lst_circularize(stack[A]); dl_lst_circularize(stack[B]);
+	pa(stack);
+	if (i_max == 0)
+	{
+		pa(stack);
+		pa(stack);
+		if (cyclic)
+			sa(stack);
+	}
+	else if (i_max == 1)
+	{
+		pa(stack);
+		sa(stack);
+		pa(stack);
+		if (!cyclic)
+			sa(stack);
+dl_lst_linearize(stack[A]); dl_lst_linearize(stack[B]); print_stacks(stack); dl_lst_circularize(stack[A]); dl_lst_circularize(stack[B]);
+	}
+	else if (i_max == 2)
+	{
+		sb(stack);
+		pa(stack);
+		sa(stack);
+		pa(stack); 
+		if (cyclic)
+			sa(stack);
+	}
 }
 
-static void	sortin__b_end(t_dl_node **stack, int *rank, size_t m, bool cyclic)
+static void	sortin__b_end(t_dl_node **stack, size_t i_max, bool cyclic)
 {
-	// rrb(stack);
-	// pa(stack);
-	// rrb(stack);
-	// pa(stack);
-	// if (((t_ps_obj *) stack[A]->obj)->n
-	// 	> ((t_ps_obj *) stack[A]->next->obj)->n)
-	// 	sa(stack);
+	if (i_max == 0)
+	{
+		rrb(stack);
+		rrb(stack);
+		rrb(stack);
+		sortin__b_top(stack, i_max, cyclic);
+	}
+	else if (i_max == 1)
+	{
+		rrb(stack);
+		rrb(stack);
+		pa(stack);
+		if (!cyclic)
+			pa(stack);
+		rrb(stack);
+		if (cyclic)
+			pa(stack);
+		pa(stack);
+	}
+	else if (i_max == 2)
+	{
+		rrb(stack);
+		pa(stack);
+		rrb(stack);
+		pa(stack);
+		rrb(stack);
+		pa(stack);
+		if (!cyclic)
+			sa(stack);
+	}
 }
