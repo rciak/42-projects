419693bbdf2f9f6bdcdc0dc8f004b8c34b9d6721
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2025-08-08 21:08:36 +0200

    Ci2-push_swap|fix: Bug fixed in is_cyclic: Now implemented for groups
    that are not the whole stack ...

--- Ci2-push_swap/src/group_tools/is_cyclic.c
+++ Ci2-push_swap/src/group_tools/is_cyclic.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 19:10:19 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/04 19:37:07 by reciak           ###   ########.fr       */
+/*   Updated: 2025/08/08 20:52:34 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,36 @@
  *        rank sequence 1 2 3 4,  2 3 4 1, 3 4 1 2, 4 1 2 3 are all cyclic,
  *        whereas 4 3 2 1, 1 4 2 3 are not
  * @note A group of size `1` or `2` is always cyclic.
- * @param[in] node A pointer to any group node
+ * @param[in] node A pointer to the **first** node of a group
  * @return 
  *          * true, if the group is cyclic
  *          * false, if the group is not cyclic
  */
-bool	is_cyclic(t_dl_node *node)
+bool	is_cyclic(t_dl_node *first)
 {
 	int	size;
+	int very_first_val;
 	int	i;
 	int a;
 	int b;
 
-	size = group_size(node);
+	size = group_size(first);
 	if (size == 1 || size == 2)
 		return (true);
+	very_first_val = ((t_ps_obj *) first->obj)->group.rank;
 	i = 0;
-	while (i < size)
+	while (i < size - 1)
 	{
-		a = ((t_ps_obj *) node->obj)->group.rank;
-		b = ((t_ps_obj *) node->next->obj)->group.rank;
-		if ((b - a) % size != 1 && ((b - a) % size ) + size != 1)
+		a = ((t_ps_obj *) first->obj)->group.rank;
+		b = ((t_ps_obj *) first->next->obj)->group.rank;
+		if ((b - a + size) % size != 1)
 			return (false);
-		node = node->next;
+		first = first->next;
 		i++;
 	}
+	a = b;
+	b = very_first_val;
+	if ((b - a + size) % size != 1)
+		return (false);
 	return (true);
 }
