f4c971c76751499236c1029909813009ae565b58
Rene Angelo Ciak <reciak@c3r2p2.42vienna.com>
2025-09-02 16:17:08 +0200

    Ci2-fractol|style: Better error message

--- Ci2-fractol/src/error.c
+++ Ci2-fractol/src/error.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 09:19:29 by reciak            #+#    #+#             */
-/*   Updated: 2025/09/01 12:27:29 by reciak           ###   ########.fr       */
+/*   Updated: 2025/09/02 15:14:31 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ t_err	error(int error_code)
 {
 	static const t_err	err[] = {
 	{ERR_NONE, "fractol: Success - no error detected"},
-	{ERR_ARG, "fractol: Bad arguments."},
+	{ERR_ARG, "fractol: Bad arguments. Try e.g."
+		GREEN"\n   ./fractol Julia 0.73 0.20"RESET
+	    "\n(Enter at most 4 or 5 fractional digits)"},
 	{ERR_ATOF_STRICT, "fractol: atof_strict from libft gave an error"},
 	{ERR_MLX_INIT, "fractol: mlx_init failed"},
 	{ERR_MLX_NEW_WINDOW, "fractol: mlx_new_window failed"},
