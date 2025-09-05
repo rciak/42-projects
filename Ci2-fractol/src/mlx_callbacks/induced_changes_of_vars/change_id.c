febf786d37a1ab0a5027de6f45c5cd84a6663126
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2025-09-05 18:51:15 +0200

    Ci2-fractol|refactor: change_id.c  (also now normconform)

--- Ci2-fractol/src/mlx_callbacks/induced_changes_of_vars/change_id.c
+++ Ci2-fractol/src/mlx_callbacks/induced_changes_of_vars/change_id.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 12:00:55 by reciak            #+#    #+#             */
-/*   Updated: 2025/09/05 12:43:22 by reciak           ###   ########.fr       */
+/*   Updated: 2025/09/05 18:50:19 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
  */
 
 #include "fractol.h"
+
+static void	set__them(char **id, char *str,
+				t_math *math, t_cmplx (*fun)(t_cmplx, t_cmplx));
 
 /**
  * @brief Changes the current type of fractal
@@ -27,51 +30,27 @@
 void	change_id(int key, char **id, t_math *math)
 {
 	if (key == XK_2)
-	{
-		*id = "m2";
-		math[MBROT].iter_fun = &z_pow_2_plus_w;
-		math[JULIA].iter_fun = &z_pow_2_plus_w;
-	}
+		set__them(id, "m2", math, &z_pow_2_plus_w);
 	else if (key == XK_3)
-	{
-		*id = "m3";
-		math[MBROT].iter_fun = &z_pow_3_plus_w;
-		math[JULIA].iter_fun = &z_pow_3_plus_w;
-	}
+		set__them(id, "m3", math, &z_pow_3_plus_w);
 	else if (key == XK_4)
-	{
-		*id = "m4";
-		math[MBROT].iter_fun = &z_pow_4_plus_w;
-		math[JULIA].iter_fun = &z_pow_4_plus_w;
-	}
+		set__them(id, "m4", math, &z_pow_4_plus_w);
 	else if (key == XK_5)
-	{
-		*id = "m5";
-		math[MBROT].iter_fun = &z_pow_5_plus_w;
-		math[JULIA].iter_fun = &z_pow_5_plus_w;
-	}
+		set__them(id, "m5", math, &z_pow_5_plus_w);
 	else if (key == XK_6)
-	{
-		*id = "m6";
-		math[MBROT].iter_fun = &z_pow_6_plus_w;
-		math[JULIA].iter_fun = &z_pow_6_plus_w;
-	}
+		set__them(id, "m6", math, &z_pow_6_plus_w);
 	else if (key == XK_7)
-	{
-		*id = "m7";
-		math[MBROT].iter_fun = &z_pow_7_plus_w;
-		math[JULIA].iter_fun = &z_pow_7_plus_w;
-	}
+		set__them(id, "m7", math, &z_pow_7_plus_w);
 	else if (key == XK_8)
-	{
-		*id = "m8";
-		math[MBROT].iter_fun = &z_pow_8_plus_w;
-		math[JULIA].iter_fun = &z_pow_8_plus_w;
-	}
+		set__them(id, "m8", math, &z_pow_8_plus_w);
 	else if (key == XK_9)
-	{
-		*id = "m9";
-		math[MBROT].iter_fun = &z_pow_9_plus_w;
-		math[JULIA].iter_fun = &z_pow_9_plus_w;
-	}
+		set__them(id, "m9", math, &z_pow_9_plus_w);
+}
+
+static void	set__them(char **id, char *str,
+				t_math *math, t_cmplx (*fun)(t_cmplx, t_cmplx))
+{
+	*id = str;
+	math[MBROT].iter_fun = fun;
+	math[JULIA].iter_fun = fun;
 }
