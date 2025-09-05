04df5d79c313b2149686f9bc8721ca3f3755ae68
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2025-09-05 11:30:26 +0200

    Ci2-fractol|refactor: Made z_pow_3_plus_w.c more readable
    It shall also be used a kind of template to give to AI
    to write similar code for (x+i)^n + for n = 4, 5, 6, 7, 8, 9

--- Ci2-fractol/src/math/fractal_genes/z_pow_3_plus_w.c
+++ Ci2-fractol/src/math/fractal_genes/z_pow_3_plus_w.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 19:17:05 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/31 22:41:50 by reciak           ###   ########.fr       */
+/*   Updated: 2025/09/05 11:29:35 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,19 @@
  *          C x C   --> C
  *          (z, w) |--> z^3 + w
  */
-
 t_cmplx	z_pow_3_plus_w(t_cmplx z, t_cmplx w)
 {
 	t_cmplx	zeta;
+	double	x;
+	double	y;
+	double	u;
+	double	v;
 
-	zeta.re = z.re * z.re * z.re - 3 * z.re * z.im * z.im + w.re;
-	zeta.im = 3 * z.re * z.re * z.im - z.im * z.im * z.im + w.im;
+	x = z.re;
+	y = z.im;
+	u = w.re;
+	v = w.im;
+	zeta.re = x * x * x - 3 * x * y * y + u;
+	zeta.im = 3 * x * x * y - y * y * y + v;
 	return (zeta);
 }
