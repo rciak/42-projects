d4fc0dbfee8c9bf1d15151701077e7a4fd754adb
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2025-09-06 21:32:55 +0200

    Ci2-fractol|doc: Completed doc of change_julia_param

--- Ci2-fractol/src/mlx_callbacks/induced_changes_of_vars/change_julia_param.c
+++ Ci2-fractol/src/mlx_callbacks/induced_changes_of_vars/change_julia_param.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 20:49:52 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/31 22:52:59 by reciak           ###   ########.fr       */
+/*   Updated: 2025/09/06 21:31:31 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,10 @@
  * @param[in] k The horizontal pixel coordinate
  * @param[in] l The vertical pixel coordinate
  * @param[out] w_0 The complex number used as Julia parameter
+ * @param[in] square Essentially the view range (from Window MBROT!) that
+ *                   determines which complex numbers are corresponding to
+ *                   which pixel.
  */
-
 void	change_julia_param(int k, int l, t_cmplx *w_0, t_square square)
 {
 	*w_0 = coord_transf(k, l, square);
