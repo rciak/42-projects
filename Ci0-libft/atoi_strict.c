1a199d5cfc34667c64db093dcf344aafcbdecfba
Rene Angelo Ciak <reciak@c1r7p3.42vienna.com>
2025-09-11 10:33:02 +0200

    Ci0-libft|maintainance_1: Compared and handmerged Ci0-libft <-- Ci2-fractol/libft

--- Ci0-libft/atoi_strict.c
+++ Ci0-libft/atoi_strict.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 11:29:50 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/10 16:51:56 by reciak           ###   ########.fr       */
+/*   Updated: 2025/08/15 12:02:35 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,6 @@ static bool	out__of_range(int sign, long long nbr, t_libft_err *err_code);
  * @return 
  *          * 0 on error
  *          * the converted number (potential 0 as well)
- * @warning **Test Status:** functionality (but not err_codes) indirectly
- *          tested by Tripouille's libftTester via the wrapper
-   @code
-   int	ft_atoi(const char *nptr)
-   {
-      t_libft_err	err_code;
-
-      return (atoi_proper(nptr, &err_code));
-   }
-   @endcode
  */
 int	atoi_strict(const char *nptr, t_libft_err *err_code)
 {
@@ -71,7 +61,7 @@ int	atoi_strict(const char *nptr, t_libft_err *err_code)
 	}
 	if (!only__whitespace(nptr, err_code) || out__of_range(sign, nbr, err_code))
 		return (0);
-	return (*err_code = E_ATOI_NOERR, (int)(sign * nbr));
+	return (*err_code = E_ATOI_NO_ERR, (int)(sign * nbr));
 }
 
 static bool	only__whitespace(const char *nptr, t_libft_err *err_code)
--- Ci0-libft/libft.h
+++ Ci0-libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 20:42:14 by reciak            #+#    #+#             */
-/*   Updated: 2025/08/09 11:29:41 by reciak           ###   ########.fr       */
+/*   Updated: 2025/09/07 10:26:21 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,14 @@
 # include <stdbool.h>  // true, false
 # include <stdlib.h>   // malloc, free   (also NULL definition aside stddef.h)
 # include <stdint.h>   // intptr_t, uintptr_t
-# include <limits.h>   // UINT_MAX
+# include <values.h>   // Indirectly includes
+//                          limits.h --> UINT_MAX
+//                          float.h  --> DBL_MIN, DBL_MAX
+// Note: The following caused norminette to print an "error" in red,
+//       likely its parser does not deal properly with  float.h  ,
+//       messing it up with the keyword  float ?!
+//# include <float.h>    // DBL_MIN, DBL_MAX
+//# include <limits.h>   // UINT_MAX
 
 # define RESET "\033[0m"
 # define RED "\033[31m"
@@ -31,13 +38,26 @@
 # define BLUE "\033[34m"
 # define CYAN "\033[36m"
 
+/**
+ * @warning The error code `E_ATOI_...`, `E_ATOLL_...`, `E_ATOF_...` are
+ *          related to each other. Their relative differences should not 
+ *          changed, since this will break the logic in 
+ *          `atoll__err_to atof_err`, c.f. atof_stric().
+ */
 typedef enum e_libft_err
 {
 	E_ARG_VAL = -1,
 	E_WRITE = -10,
-	E_ATOI_NOERR = -20,
+	E_ATOI_NO_ERR = -20,
 	E_ATOI_BAD_STRING = -21,
-	E_ATOI_RANGE = -22
+	E_ATOI_RANGE = -22,
+	E_ATOLL_NO_ERR = -30,
+	E_ATOLL_BAD_STRING = -31,
+	E_ATOLL_RANGE = -32,
+	E_ATOF_NO_ERR = -40,
+	E_ATOF_BAD_STRING = -41,
+	E_ATOF_RANGE_PANIC = -42,
+	E_ATOF_MALLOC = -44,
 }				t_libft_err;
 
 typedef struct s_list
@@ -104,6 +124,7 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+void		ft_putllnbr_fd(long long n, int fd);
 
 // Part 3: Bonus
 t_list		*ft_lstnew(void *content);
@@ -162,8 +183,26 @@ size_t		dl_lst_size(const t_dl_node *const pdl_node);
 t_dl_type	dl_lst_type(const t_dl_node *const pdl_node);
 void		dl_lst_clear(t_dl_node **addr_pdl_node, void (*del)(void*));
 
+// Taken from Piscine / adopted:
+int			ft_strcmp(const char *s1, const char *s2);
+
+//currently in ft_x_strtools1.c
+size_t		skip(char **pstr, const char *chars_to_skip);
+
+//currently in sideproducts/ft_x_strtools2.c
+size_t		strlen_deli(const char *s, const char *deli);
+size_t		count_words(const char *s, const char *deli);
+char		**split_deli(char const *s, const char *deli);
+
 //ft_atoi with error info (with additional strictness in case of atoi_strict)
-int			atoi_proper(const char *nptr, t_libft_err *err);
-int			atoi_strict(const char *nptr, t_libft_err *err);
+int			atoi_proper(const char *nptr, t_libft_err *err_code);
+int			atoi_strict(const char *nptr, t_libft_err *err_code);
+
+long long	atoll_strict(const char *nptr, t_libft_err *err_code);
+double		atof_strict(const char *nptr, t_libft_err *err_code);
+
+bool		is_listed(const char *str, const char **list);
+
+void		free_array(char **string_array);
 
 #endif
