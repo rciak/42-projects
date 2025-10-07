6833df9778049bdd2548abf94c751656d7e753d7
Rene Angelo Ciak <reciak@c3r2p2.42vienna.com>
2025-10-07 13:55:26 +0200

    Ci2-pipex|built: Tidied up doc.mk

--- Ci2-pipex/makefile_dir/rules/doc.mk
+++ Ci2-pipex/makefile_dir/rules/doc.mk
@@ -6,7 +6,7 @@
 #    By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/10/06 18:24:11 by reciak            #+#    #+#              #
-#    Updated: 2025/10/06 18:26:13 by reciak           ###   ########.fr        #
+#    Updated: 2025/10/06 19:38:39 by reciak           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,32 +17,32 @@ $(error CHECK_VARIABLES must be defined by the including makefile!)
 endif
 include $(CHECK_VARIABLES)
 
-DOC_DIR:=doc
-INC_DIR:=inc
-SRC_DIR:=src
+DOXY_OUT_DIR := doc
+DOXY_IN_DIRS := inc src
 
-DOXYFILE=$(DOC_DIR)/autogen_Doxyfile
+DOXYFILE=$(DOXY_OUT_DIR)/autogen_Doxyfile
 # For sed substitions in the Doxyfile
 SED_PROJECT_NAME=         PROJECT_NAME           = $(PROJECT_NAME)
-SED_INPUT=                INPUT                  = $(INC_DIR) $(SRC_DIR)
-SED_OUTPUT_DIRECTORY=     OUTPUT_DIRECTORY       = $(DOC_DIR)
+SED_OUTPUT_DIRECTORY=     OUTPUT_DIRECTORY       = $(DOXY_OUT_DIR)
+SED_INPUT=                INPUT                  = $(DOXY_IN_DIRS)
+SED_RECURSIVE=            RECURSIVE              = YES
 SED_OPTIMIZE_OUTPUT_FOR_C=OPTIMIZE_OUTPUT_FOR_C  = YES
 SED_SOURCE_BROWSER=       SOURCE_BROWSER         = YES
-SED_RECURSIVE=            RECURSIVE              = YES
 
-$(DOC_DIR): $(SRCS) $(SRCS_BONUS) $(INCS)
-	@mkdir -p $(DOC_DIR)
+
+$(DOXY_OUT_DIR): $(SRCS) $(SRCS_BONUS) $(INCS)
+	@mkdir -p $(DOXY_OUT_DIR)
 	doxygen -g $(DOXYFILE)
 	sed -i 's#PROJECT_NAME           =.*#$(SED_PROJECT_NAME)#'     $(DOXYFILE)
-	sed -i 's#INPUT                  =.*#$(SED_INPUT)#'            $(DOXYFILE)
 	sed -i 's#OUTPUT_DIRECTORY       =.*#$(SED_OUTPUT_DIRECTORY)#' $(DOXYFILE)
+	sed -i 's#INPUT                  =.*#$(SED_INPUT)#'            $(DOXYFILE)
+	sed -i 's#RECURSIVE              =.*#$(SED_RECURSIVE)#'        $(DOXYFILE)
 	sed -i 's#OPTIMIZE_OUTPUT_FOR_C  =.*#$(SED_OPTIMIZE_OUTPUT_FOR_C)#' $(DOXYFILE)
 	sed -i 's#SOURCE_BROWSER         =.*#$(SED_SOURCE_BROWSER)#'   $(DOXYFILE)
-	sed -i 's#RECURSIVE              =.*#$(SED_RECURSIVE)#'        $(DOXYFILE)
 	doxygen $(DOXYFILE)
 
 .PHONY: docre
-docre: docfclean $(DOC_DIR)
+docre: docfclean $(DOXY_OUT_DIR)
 
 .PHONY: docclean
 docclean:
@@ -50,5 +50,5 @@ docclean:
 
 .PHONY: docfclean
 docfclean: docclean
-	rm -fr $(DOC_DIR)
+	rm -fr $(DOXY_OUT_DIR)
 
