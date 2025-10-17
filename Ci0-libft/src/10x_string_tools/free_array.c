b964f2b400b63ad56c02cba3b5f411aa4f567227
Rene Angelo Ciak <reciak@c1r1p10.42vienna.com>
2025-10-17 12:01:41 +0200

    Ci2-pipex/libft|doc: Added remark in doxy comment of free_array about its todays bugfix

--- Ci0-libft/src/10x_string_tools/free_array.c
+++ Ci0-libft/src/10x_string_tools/free_array.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 14:12:45 by reciak            #+#    #+#             */
-/*   Updated: 2025/10/17 11:48:12 by reciak           ###   ########.fr       */
+/*   Updated: 2025/10/17 12:01:10 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@
  * @note Typical used to free the allocations done by ft_split().
  * @param[in] string_array Containing pointers to the allocated strings.
  * @remark (17.10.2025) Found and fixed a bug that was in till commit
- *         3a5f6e5a4109daf61fec0bc4b86b671e9bd550c3    (branch  Ci2-pipex)
+ *           3a5f6e5a4109daf61fec0bc4b86b671e9bd550c3    (branch  Ci2-pipex)
+ *         This fix was also commited in the main branch at commit
+ *           3bb347579ba2f66aa53e8b5a73477807a88cdaa1
  */
 void	free_array(char **string_array)
 {
