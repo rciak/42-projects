3b8dab26aa5e9656da203bd0922398be75e3a9a4
Rene Angelo Ciak <reciak@c1r1p10.42vienna.com>
2025-10-21 11:21:28 +0200

    Ci2-pipex|tmp: Parsing Check: Added a quick and dirty info printing in main:
    Parsing seems to work

--- Ci2-pipex/src/main.c
+++ Ci2-pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 11:07:32 by reciak            #+#    #+#             */
-/*   Updated: 2025/10/21 11:05:22 by reciak           ###   ########.fr       */
+/*   Updated: 2025/10/21 11:19:50 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,64 @@ int	main(int argc, char **argv, char**envp)
 		|| !parse_path(envp, data.num_cmds, data.cmd, &x_err)
 		|| !exec_pipeline(data.cmd, data.num_cmds, &pid_last_cmd, &x_err))
 		return (handle_error(x_err));
+printf("data.n_cmds:      |%zu|\n", data.num_cmds);
+printf("data.cmd:         |%p|\n", data.cmd);
+printf("data.cmd->infile: |%s|\n", data.cmd[0].infile);
+printf("data.cmd->outfile:|%s|\n", data.cmd[0].outfile);
+printf("data.cmd->infile: |%s|\n", data.cmd[1].infile);
+printf("data.cmd->outfile:|%s|\n", data.cmd[1].outfile);
+printf("data.cmd->av:     |%p|\n", data.cmd[0].av);
+printf("                  |%s|\n", data.cmd[0].av[0]);
+printf("                  |%s|\n", data.cmd[0].av[1]);
+printf("data.cmd->av:     |%p|\n", data.cmd[0].av);
+printf("                  |%s|\n", data.cmd[1].av[0]);
+printf("                  |%s|\n", data.cmd[1].av[1]);
+printf("data.cmd->path:   |%p|\n", data.cmd[0].path);
+printf("                  |%s|\n", data.cmd[1].path[0]);
+printf("                  |%s|\n", data.cmd[1].path[1]);
+printf("                  |%s|\n", data.cmd[1].path[1]);
+printf("                  |%s|\n", data.cmd[1].path[2]);
+printf("                  |%s|\n", data.cmd[1].path[3]);
+printf("                  |%s|\n", data.cmd[1].path[4]);
+printf("                  |%s|\n", data.cmd[1].path[5]);
+printf("                  |%s|\n", data.cmd[1].path[6]);
+printf("                  |%s|\n", data.cmd[1].path[7]);
+printf("                  |%s|\n", data.cmd[1].path[8]);
+printf("                  |%s|\n", data.cmd[1].path[9]);
+
+printf("                  |%s|\n", data.cmd[1].path[10]);
+printf("                  |%s|\n", data.cmd[1].path[11]);
+printf("                  |%s|\n", data.cmd[1].path[11]);
+printf("                  |%s|\n", data.cmd[1].path[12]);
+printf("                  |%s|\n", data.cmd[1].path[13]);
+printf("                  |%s|\n", data.cmd[1].path[14]);
+printf("                  |%s|\n", data.cmd[1].path[15]);
+printf("                  |%s|\n", data.cmd[1].path[16]);
+printf("                  |%s|\n", data.cmd[1].path[17]);
+printf("                  |%s|\n", data.cmd[1].path[18]);
+printf("                  |%s|\n", data.cmd[1].path[19]);
+printf("                  |%s|\n", data.cmd[1].path[20]);
+printf("                  |%s|\n", data.cmd[1].path[21]);
+printf("                  |%s|\n", data.cmd[1].path[21]);
+printf("                  |%s|\n", data.cmd[1].path[22]);
+printf("                  |%s|\n", data.cmd[1].path[23]);
+printf("                  |%s|\n", data.cmd[1].path[24]);
+printf("                  |%s|\n", data.cmd[1].path[25]);
+printf("                  |%s|\n", data.cmd[1].path[26]);
+printf("                  |%s|\n", data.cmd[1].path[27]);
+printf("                  |%s|\n", data.cmd[1].path[28]);
+printf("                  |%s|\n", data.cmd[1].path[29]);
+
+printf("                  |%s|\n", data.cmd[1].path[30]);
+printf("                  |%s|\n", data.cmd[1].path[31]);
+printf("                  |%s|\n", data.cmd[1].path[31]);
+printf("                  |%s|\n", data.cmd[1].path[32]);
+printf("                  |%s|\n", data.cmd[1].path[33]);
+printf("                  |%s|\n", data.cmd[1].path[34]);
+printf("                  |%s|\n", data.cmd[1].path[35]);
+printf("                  |%s|\n", data.cmd[1].path[36]);
+printf("                  |%s|\n", data.cmd[1].path[37]);
+printf("                  |%s|\n", data.cmd[1].path[38]);
 	termination_status_last_cmd = wait__without_creating_zombies(pid_last_cmd);
 	final_free(data);
 	return (termination_status_last_cmd);
