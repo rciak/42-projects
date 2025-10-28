8f9858239915ee1078116676c4ad4308ba720b26
Rene Angelo Ciak <reciak@c3r6p8.42vienna.com>
2025-10-28 17:37:37 +0100

    Ci2-pipex|feat: Implemented separate error handling for E_FUN_ASSERTION

--- Ci2-pipex/src/a_col_exiting/h_err_exit.c
+++ Ci2-pipex/src/a_col_exiting/h_err_exit.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 07:21:36 by reciak            #+#    #+#             */
-/*   Updated: 2025/10/28 16:04:41 by reciak           ###   ########.fr       */
+/*   Updated: 2025/10/28 17:34:40 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,44 @@
 #include "pipex.h"
 
 static void	default__err_handling(size_t num_cmds, t_cmd *cmd, t_err *err);
+static void	assertion__err_handling(size_t num_cmds, t_cmd *cmd, t_err *err);
 
 /**
  * @brief Handles the passed error and exits
  */
 void	h_err_exit(size_t num_cmds, t_cmd *cmd, t_err *err)
 {
-	default__err_handling(num_cmds, cmd, err);
+	if (err->type == E_FUN_ASSERTION)
+		assertion__err_handling(num_cmds, cmd, err);
+	else
+		default__err_handling(num_cmds, cmd, err);
+}
+
+static void	assertion__err_handling(size_t num_cmds, t_cmd *cmd, t_err *err)
+{
+	t_err	copy;
+	size_t	i;
+
+	copy = *err;
+	i = 0;
+	while (i < num_cmds)
+	{
+		tidy_up_and_reset_cmd_items(&cmd[i], err);
+		i++;
+	}
+	free (cmd);
+	if (err->type != copy.type)
+		out_str_fd(BLUE"What?! Even on tidying up another error happend?!\n"
+			"Not investigating that (*). Just reporting the previous issue..\n"
+			"\n"
+			"REMARK to (*) : Who came up with the 'good' idea of cleaning\n"
+			"before exiting although the OS can do this better? ;-) )\n\n"RESET,
+			STDERR_FILENO);
+	out_str_fd("\nExiting ... ", STDERR_FILENO);
+	out_str_fd("\n  --> Assertion in function  ", STDERR_FILENO);
+	out_str_fd((char *) err->origin, STDERR_FILENO);
+	out_str_fd("  failed\n", STDERR_FILENO);
+	exit(err->exit.code);
 }
 
 static void	default__err_handling(size_t num_cmds, t_cmd *cmd, t_err *err)
