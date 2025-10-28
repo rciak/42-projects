419a4dc886c8985cef2206afc1ba35da2456e259
Rene Angelo Ciak <reciak@c3r6p8.42vienna.com>
2025-10-28 17:47:36 +0100

    Ci2-pipex|style: Extended printing in logig_error_exit

--- Ci2-pipex/src/a_col_exiting/logic_error_exit.c
+++ Ci2-pipex/src/a_col_exiting/logic_error_exit.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 16:57:28 by reciak            #+#    #+#             */
-/*   Updated: 2025/10/27 15:53:13 by reciak           ###   ########.fr       */
+/*   Updated: 2025/10/28 17:46:50 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,12 @@
 void	logic_error_exit(const char *msg)
 {
 	out_str_fd("Exiting directly because of LOGIC ERROR: \n  ", STDERR_FILENO);
-	out_str_fd((char*) msg, STDERR_FILENO);
+	out_str_fd((char *) msg, STDERR_FILENO);
+	out_str_fd("\nAnd yes I still think it is nonsense\n"
+		"to call a tidy up function before exiting and that,"
+		"given the proper understanding of a memory leak"
+		"not doing so does NOT CAUSE memory leaks..."
+		"Let me know if you are interested in more details ;-)",
+		STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
