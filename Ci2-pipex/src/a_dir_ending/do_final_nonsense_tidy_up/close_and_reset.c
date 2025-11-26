8a286c5c567ac132b8e5863417693842e6df06bc
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2025-11-26 08:22:47 +0100

    Ci2-pipex|feat: Helpers for tidying up, taken and adopted from Ansatz 2

---
+++ Ci2-pipex/src/a_dir_ending/do_final_nonsense_tidy_up/close_and_reset.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   close_and_reset.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 09:48:37 by reciak            #+#    #+#             */
+/*   Updated: 2025/11/26 08:21:42 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file close_and_reset.c
+ * @brief Stores the definition of close_and_reset()
+ */
+
+#include "pipex.h"
+
+/**
+ * @brief Closes a non negative filedescriptor and sets it to UNUSED (== -1 < 0)
+ * @param[in,out] fd A pointer to a file descriptor
+ */
+void	close_and_reset(int *fd)
+{
+	if (*fd < 0)
+	{
+		out_str_fd(RED"Warning"RESET
+			" - close_and_reset: ignore filedescriptor < 0 \n", STDERR_FILENO);
+		return ;
+	}
+	if (*fd == STDIN_FILENO)
+		out_str_fd(RED"Warning"RESET
+			" - close_and_reset:closing standard fd for input", STDERR_FILENO);
+	if (*fd == STDOUT_FILENO)
+		out_str_fd(RED"Warning"RESET
+			" - close_and_reset:closing standard fd for output", STDERR_FILENO);
+	if (close(*fd) == -1)
+	{
+		out_str_fd(RED"Warning"RESET
+			" - close_and_reset: close failed\n", STDERR_FILENO);
+		return ;
+	}
+	*fd = UNUSED;
+	return ;
+}
