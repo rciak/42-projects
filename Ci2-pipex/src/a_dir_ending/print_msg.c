725e3088adf67e0b07fba6032e44b068da811da5
Rene Ciak <rciakAT42Vienna@web.de>
2025-11-28 13:19:25 +0100

    Ci2-pipex|transport

--- Ci2-pipex/src/a_dir_ending/print_msg.c
+++ Ci2-pipex/src/a_dir_ending/print_msg.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 12:53:28 by reciak            #+#    #+#             */
-/*   Updated: 2025/11/28 10:33:32 by reciak           ###   ########.fr       */
+/*   Updated: 2025/11/28 13:17:25 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 #include "pipex.h"
 
-static void	treat_str2(t_exit_info info, const char *origin, t_data *data);
+static char *interpretation__str2(t_exit_info info, t_data *data);
 
 /**
  * @brief Print a message on exititing
@@ -29,21 +29,60 @@ static void	treat_str2(t_exit_info info, const char *origin, t_data *data);
 
 void	print_msg(t_exit_info info, const char *origin, t_data *data)
 {
-	out_str_fd("\nExiting ... ", STDERR_FILENO);
-	out_str_fd("\n  --> ", STDERR_FILENO);
-	out_str_fd(info.str1, STDERR_FILENO);
-	out_str_fd(" ", STDERR_FILENO);
-	treat_str2(info, origin, data);
-	out_str_fd("\n", STDERR_FILENO);
+	char	*str2_final;
+	char	*msg;
+	
+	str2_final = treat__str2(info, data);
+	if (str2_final == NULL)
+		exit_on(E_ALLOC, errno, "print_msg", data);
+	msg = NULL;
+	if (!add_to_str(&msg, "\nExiting ... \n  --> ")
+		|| !add_to_str(&msg, info.str1)
+		|| !add_to_str(&msg, " ")
+		|| !add_to_str(&msg, str2_final)
+		|| !add_to_str(&msg, "           (origin: ")
+		|| !add_to_str(&msg, origin)
+		|| !add_to_str(&msg, ")\n")
+	)
+	{
+		free(str2_final);
+		free(msg);
+		exit_on(E_ALLOC, errno, "print_msg", data);
+	}
+	out_str_fd(msg, STDERR_FILENO);
+	free(str2_final);
+	free(msg);
 }
 
-static void	treat_str2(t_exit_info info, const char *origin, t_data *data)
+static bool add__to_str(char **str, const char *post_fix)
+{
+	char	*new;
+	
+	if (*str == NULL)
+		new = ft_strdup(post_fix);
+	else
+		new = ft_strjoin(*str, post_fix);
+	if (new == NULL)
+		return (false);
+	free(*str);
+	*str = new;
+	return (true);
+}
+
+static char *interpretation__str2(t_exit_info info, t_data *data)
 {
 	t_cmd	*cmd;
 	int		i;
+	char *interpreted;
 
 	cmd = data->cmd;
 	i = data->i_cmd_err;
+	interpreted = NULL;
+
+
+	
+	return (interpreted);
+}
 	if (ft_strcmp(info.str2, "cmd[i].av[0]") == 0)
 		out_str_fd(cmd[i].av[0], STDERR_FILENO);
 	else if (ft_strcmp(info.str2, "cmd[i].infile") == 0)
