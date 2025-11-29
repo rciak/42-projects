e03b6a1bdb960edb02e2eb9d8b17c3f2c529aa4e
René Angelo Ciak <reciak@c3r2p2.42vienna.com>
2025-11-29 19:56:39 +0100

    Ci2-pipex|fix: Logical error  ( != --> == )

--- Ci2-pipex/src/exec_pipeline/exec__cmd/set_pathname_as_av0.c
+++ Ci2-pipex/src/exec_pipeline/exec__cmd/set_pathname_as_av0.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 13:43:55 by reciak            #+#    #+#             */
-/*   Updated: 2025/11/29 14:49:46 by reciak           ###   ########.fr       */
+/*   Updated: 2025/11/29 19:54:10 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ void	set_pathname_as_av0(t_data *data, int i)
 	data->cmd[i].pathname = ft_strdup(data->cmd[i].av[0]);
 	if (data->cmd[i].pathname == NULL)
 		exit_on(E_ALLOC, errno, "set_pathname_as_av0", data);
-	if (access(data->cmd[i].pathname, F_OK) != -1)
+	if (access(data->cmd[i].pathname, F_OK) == -1)
 		exit_on(E_NOT_FOUND, errno, "set_pathname_as_av0", data);
-	if (access(data->cmd[i].pathname, X_OK) != -1)
+	if (access(data->cmd[i].pathname, X_OK) == -1)
 		exit_on(E_NO_PERM, errno, "set_pathname_as_av0", data);
 }
