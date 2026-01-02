c8c41661ea3ce965cc8de653a871e34fcfe8c44f
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-01-02 18:18:23 +0100

    Ci3-philosophers|infra: Copied over Makefile, makefile and makefile_dir
    from b632fbe (currently the same as: Ci2-pipex)

---
+++ Ci3-philosophers/makefile_dir/def_headers.mk
@@ -0,0 +1,7 @@
+INCS := \
+inc/debug.h \
+inc/pipex_bonus.h \
+inc/pipex.h \
+libft/inc/ft_printf.h \
+libft/inc/get_next_line.h \
+libft/inc/libft.h
---
+++ Ci3-philosophers/makefile_dir/rules/checks/check_variables.mk
@@ -0,0 +1,68 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    check_variables.mk                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/10/03 15:47:14 by reciak            #+#    #+#              #
+#    Updated: 2025/10/11 12:45:24 by reciak           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#
+# Checking of Variables related to Organization of GNU Make Code
+#
+
+ifndef INCLUDED_FROM_DIR
+$(error INCLUDED_FROM_DIR must be defined by the including makefile)
+endif
+
+ifndef THIS_FILE
+$(error THIS_FILE must be defined by all files *.mk in directoy rules)
+endif
+
+#
+# Checking of local paths to a directory or to directories
+#
+
+ifndef LIBFT_DIR
+$(error LIBFT_DIR must be defined by the including makefile)
+endif
+
+ifndef INC_DIRS
+$(error INC_DIRS must be defined by the including makefile)
+endif
+
+ifndef TESTS_DIR
+$(error TESTS_DIR must be defined by the including makefile)
+endif
+
+#
+# Checking of local paths to files
+#
+
+ifndef DEF_INCS
+$(error DEF_INCS must be defined by the including makefile)
+endif
+
+ifndef DEF_SRCS
+$(error DEF_SRCS must be defined by the including makefile)
+endif
+
+ifndef RULE_HYPERLINKS_CLANGD
+$(error file path RULE_HYPERLINKS_CLANGD not defined by the including makefile)
+endif
+
+ifndef RULE_DOCUMENTATION
+$(error file path RULE_RULE_DOCUMENTATION not defined by the including makefile)
+endif
+
+ifndef RULE_TESTS
+$(error file path RULE_TESTS not defined by the including makefile)
+endif
+
+ifndef RULE_UPDATE_AND_VIEW
+$(error file path RULE_UPDATE_AND_VIEW not defined by the including makefile)
+endif
+
