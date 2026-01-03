1a6541915c73fd3b65821fabd10bbad041fada8f
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-01-03 20:19:30 +0100

    Ci3-philosophers|fix: Some fixes: Basic structure (make ...) seems to work
    Implemented only argc check to test is running make works

--- Ci3-philosophers/philo/inc/philosophers.h
+++ Ci3-philosophers/philo/inc/philosophers.h
@@ -6,13 +6,71 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 16:48:14 by reciak            #+#    #+#             */
-/*   Updated: 2026/01/03 17:00:08 by reciak           ###   ########.fr       */
+/*   Updated: 2026/01/03 19:35:48 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-# include <pthread.h>  // pthread_create, ...
+///////////////////////////
+//                       //
+//  1.  I N C L U D E S  //
+//                       //
+///////////////////////////
+# include <pthread.h>           // pthread_create, ...
+# include <stdlib.h>
+# include <unistd.h>
+
+/////////////////////////
+//                     //
+//  2.  D E F I N E S  //
+//                     //
+/////////////////////////
+
+
+///////////////////////////////////
+//                               //
+//  3.  E N U M E R A T I O N S  //
+//                               //
+///////////////////////////////////
+
+enum e_error
+{
+	E_NONE,
+	E_ARGC,
+};
+
+/////////////////////////
+//                     //
+//  4.  S T R U C T S  //
+//                     //
+/////////////////////////
+
+typedef struct s_err
+{
+	enum e_error	err;
+	char			*msg;
+}	t_err;
+
+typedef struct s_all
+{
+	t_err		err;
+}	t_all;
+
+////////////////////////////////////////////////
+//                                            //
+//  5.  F U N C T I O N  P R O T O T Y P E S  //
+//                                            //
+////////////////////////////////////////////////
+
+// *.c
+int	main(int argc, char **argv);
+
+// error_handling/*.c
+int	herr(enum e_error err_code, const char *debug_info);
+
+//tools/*.c
+ssize_t	putstr_fd(const char *str, int fd);
 
 #endif
---
+++ Ci3-philosophers/philo/src/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/03 17:02:15 by reciak            #+#    #+#             */
+/*   Updated: 2026/01/03 19:42:34 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file main.c
+ * @brief Stores the definition of main()
+ */
+
+
+
+ 
+#include "philosophers.h"
+
+/**
+ * @brief Entry point for philiosophers
+ * @param[in] argc The number of arguments on the command line
+ * @param[in] argv The usual list of arguments on the command line
+ * @return 
+ *          * > 0 if an error occured
+ *          * 0 (E_NONE) if no error occured
+ */
+
+int	main(int argc, char **argv)
+{
+	if (argc < 1 + 3 || argc > 1 + 4)
+		return (herr(E_ARGC, ""));
+	return (herr(E_ARGC, NULL));
+	(void) argv;
+	return (0);
+}
