a7dfe3fecd0592b44e867669dde55df17e438b49
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-01-04 11:41:59 +0100

    Ci3-philosophers|style: Changed name of variable making it shorter

--- Ci3-philosophers/philo/src/herr.c
+++ Ci3-philosophers/philo/src/herr.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 17:54:03 by reciak            #+#    #+#             */
-/*   Updated: 2026/01/03 19:45:25 by reciak           ###   ########.fr       */
+/*   Updated: 2026/01/04 10:48:23 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@
 
 /**
  * @brief Prints an error message and returns an error code
- * @param[in] err_code, an error code defined in philosophers.h
+ * @param[in] code, an error code defined in philosophers.h
  * @param[in] debug_info, a debug message (might be NULL or "")
  * @param[in,out]
  * @return 
  *          * 
  *          * 
  */
-int	herr(enum e_error err_code, const char *debug_info)
+int	herr(enum e_error code, const char *debug_info)
 {
 	static t_err	err[] = {
 	{E_NONE, "No error\n"},
 	{E_ARGC, "Invalid number of arguments\n"},
 	};
 
-	putstr_fd(err[err_code].msg, STDERR_FILENO);
+	putstr_fd(err[code].msg, STDERR_FILENO);
 	if (debug_info != NULL && debug_info[0] != '\0')
 		putstr_fd(debug_info, STDERR_FILENO);
-	return (err_code);
+	return (code);
 }
