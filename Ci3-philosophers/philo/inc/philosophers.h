34083d06182a0fb180c5b260cc158ac2f167b4d7
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-01-08 00:00:04 +0100

    Ci3-philosophers|feat: Parsing
    Compiling and reads something in; not tested
    
    Also Makefile has now  DEBUG_PRINT=1  flag

--- Ci3-philosophers/philo/inc/philosophers.h
+++ Ci3-philosophers/philo/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 16:48:14 by reciak            #+#    #+#             */
-/*   Updated: 2026/01/03 19:35:48 by reciak           ###   ########.fr       */
+/*   Updated: 2026/01/07 23:59:01 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,36 @@
 //  1.  I N C L U D E S  //
 //                       //
 ///////////////////////////
+//
+//														!!! REMOVE before submission !!!
+# include "../src/debug/debug.h"
+//
+//
+
 # include <pthread.h>           // pthread_create, ...
-# include <stdlib.h>
-# include <unistd.h>
+# include <limits.h>            // LLONG_MAX
+# include <stdlib.h>            // malloc, free
+# include <unistd.h>            // STDERR_FILENO, ...
+# include <stdbool.h>           // true, false, ...
 
 /////////////////////////
 //                     //
 //  2.  D E F I N E S  //
 //                     //
 /////////////////////////
+
+// Call make with DEBUG_PRINT=1 to see debug messages
+#ifndef DEBUG_PRINT
+# define DEBUG_PRINT 0
+#endif
+
+# define ONE_HOUR_IN_MS 3600000
+# define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define CYAN "\033[36m"
 
 
 ///////////////////////////////////
@@ -35,11 +56,26 @@
 //                               //
 ///////////////////////////////////
 
-enum e_error
+/**
+ * @warning The error codes `E_ATOLL_...` have been tweaked for philosophers.
+ *          This tweaks should never be used in other projects, cf.
+ *          the warning  in libft.h  (which refers to atof_strict).
+ */
+typedef enum e_error_code
 {
 	E_NONE,
 	E_ARGC,
-};
+	E_MAX_NUM_MEALS_NEG,
+	E_NUM_PHILOS_NOT_POS,
+	E_TIME_TO_DIE_NOT_POS,
+	E_TIME_TO_EAT_NOT_POS,
+	E_TIME_TO_SLEEP_NEG,
+	E_DISRESPECT,
+	E_ATOLL_NO_ERR,
+	E_ATOLL_BAD_STRING,
+	E_ATOLL_RANGE,
+	E_COUNT_THEM_ALL,
+}	t_ecode;
 
 /////////////////////////
 //                     //
@@ -49,13 +85,21 @@ enum e_error
 
 typedef struct s_err
 {
-	enum e_error	err;
-	char			*msg;
+	t_ecode	code;
+	char	*msg;
 }	t_err;
 
+typedef struct s_param
+{
+	long long	num_philos;
+	long long	tt_die;
+	long long	tt_eat;
+	long long	tt_sleep;
+	long long	max_num_meals;
+}	t_param;
 typedef struct s_all
 {
-	t_err		err;
+	t_param	param;
 }	t_all;
 
 ////////////////////////////////////////////////
@@ -65,12 +109,20 @@ typedef struct s_all
 ////////////////////////////////////////////////
 
 // *.c
-int	main(int argc, char **argv);
+int			main(int argc, char **argv);
 
 // error_handling/*.c
-int	herr(enum e_error err_code, const char *debug_info);
+int			herr(t_ecode err_code, const char *debug_info);
 
 //tools/*.c
-ssize_t	putstr_fd(const char *str, int fd);
+size_t		skip(char **pstr, const char *chars_to_skip);
+ssize_t		putstr_fd(const char *str, int fd);
+bool		is_in(char c, const char *str);
+char		*ft_strchr(const char *s, int c);
+size_t		ft_strlen(const char *s);
+long long	atoll_strict(const char *nptr, t_ecode *err_code);
+
+
+void	print_parsed_args(t_param params);
 
 #endif
--- Ci3-philosophers/philo/src/debug/debug.h
+++ Ci3-philosophers/philo/src/debug/debug.h
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   putstr_fd.c                                        :+:      :+:    :+:   */
+/*   debug.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/03 18:30:05 by reciak            #+#    #+#             */
-/*   Updated: 2026/01/07 23:14:25 by reciak           ###   ########.fr       */
+/*   Created: 2026/01/07 23:25:37 by reciak            #+#    #+#             */
+/*   Updated: 2026/01/07 23:31:36 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * @file putstr_fd.c
- * @brief Stores the definition of putstr_fd()
- */
+#ifndef DEBUG_H
+# define DEBUG_H
 
-#include "philosophers.h"
 
-/**
- * @brief Prints a string
- * @param[in] 
- * @param[out]
- * @param[in,out]
- * @return 
- *          * 
- *          * 
- */
-ssize_t	putstr_fd(const char *str, int fd)
-{
-	return (write(fd, str, ft_strlen((char *) str)));
-}
+
+#endif
