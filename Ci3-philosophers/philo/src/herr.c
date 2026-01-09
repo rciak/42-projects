9eb9089539939d4ef5a7cd3661f72f4de58d1785
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-01-09 23:39:45 +0100

    Ci3-philosophers|style: Changed some error messages

--- Ci3-philosophers/philo/src/herr.c
+++ Ci3-philosophers/philo/src/herr.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 17:54:03 by reciak            #+#    #+#             */
-/*   Updated: 2026/01/09 12:38:55 by reciak           ###   ########.fr       */
+/*   Updated: 2026/01/09 20:30:04 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,15 @@ int	herr(t_ecode code, const char *debug_info)
 	static t_err	err[] = {
 	{E_NONE, "No error\n"},
 	{E_ARGC, "Invalid number of arguments\n"},
-	{E_MAX_NUM_MEALS_NEG, "Sorry: The cook will get angry if a negative number "
-		"of meals is ordered ...\n"},
+	{E_MAX_NUM_MEALS_NEG, "Number of meals must be at least 0 (or omitted)\n"},
 	{E_NUM_PHILOS_NOT_POS, "Number of philosophers must be at least 1\n"},
 	{E_TIME_TO_DIE_NOT_POS, "Time to die must be at least 1 (ms)\n"},
 	{E_TIME_TO_EAT_NOT_POS, "Time to eat must be at least 1 (ms)\n"},
 	{E_TIME_TO_SLEEP_NEG, "Time to sleep must be at least 0 (ms)\n"},
-	{E_DISRESPECT, "Sorry - philosophers do not support masochism / sadism\n"},
+	{E_DISRESPECT, "Sorry - philosophers do not support sadistic values\n"},
 	{E_ATOLL_NO_ERR, "I would like to be printed but no pointer leads to me\n"},
-	{E_ATOLL_BAD_STRING, "I need integer numbers to do my job!\n"},
-	{E_ATOLL_RANGE, "Sorry the number would be out of range ...\n"},
+	{E_ATOLL_BAD_STRING, "Atoll: I need integer numbers to do my job!\n"},
+	{E_ATOLL_RANGE, "Atoll: Sorry the number would be out of range ...\n"},
 	};
 
 	if (code < 0 || code >= E_COUNT_THEM_ALL)
