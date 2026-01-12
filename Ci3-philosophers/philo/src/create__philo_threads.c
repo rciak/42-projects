d110fac84d6bbdde4dadcba10bffbdc56aff5ac4
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-01-12 19:08:12 +0100

    Ci3-philosophers|transport: Got a call ...

---
+++ Ci3-philosophers/philo/src/create__philo_threads.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create__philo_threads.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 17:51:08 by reciak            #+#    #+#             */
+/*   Updated: 2026/01/12 19:07:43 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file create__philo_threads.c
+ * @brief Stores the definition of create__philo_threads()
+ */
+
+#include "philosophers.h"
+
+/**
+ * @brief Creates the philosopher threads and inits the philo array
+ * @param[in] 
+ * @param[out]
+ * @param[in,out]
+ * @return 
+ *          * 
+ *          * 
+ */
+bool	create__philo_threads(t_all *all, t_ecode *code)
+{
+	long long	i;
+
+	i = 0;
+	while (i < all->param.num_philos)
+	{
+
+		// all->philo[i].id = i + 1;
+		// all->philo[i].latest_meal = ;
+		// all->philo[i].ended_meals = ;
+		pthread_create(&all->philo[i].thread, NULL, &philo_fun, (void *)all);
+		i++;
+	}
+	return (true);
+}
