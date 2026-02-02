b8e1cdd49429b78069a274798757acd801902497
René Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-02-02 10:59:32 +0100

    Ci3-philosophers|refactor: Moved parsing from main.c to parse_args.c
    makeing prev. static helper of main non-static

--- Ci3-philosophers/philo/makefile_dir/def_sources.mk
+++ Ci3-philosophers/philo/makefile_dir/def_sources.mk
@@ -6,6 +6,7 @@ src/herr.c \
 src/herr_free.c \
 src/init_rest.c \
 src/main.c \
+src/parse_args.c \
 src/philo_fun.c \
 src/tools_libft/atoll_strict.c \
 src/tools_libft/ft_strchr.c \
--- Ci3-philosophers/philo/src/main.c
+++ Ci3-philosophers/philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 17:02:15 by reciak            #+#    #+#             */
-/*   Updated: 2026/01/16 18:18:43 by reciak           ###   ########.fr       */
+/*   Updated: 2026/02/02 10:53:26 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@
 
 #include "philosophers.h"
 
-static bool	parse__args(int argc, char **argv, t_param *param, t_ecode *code);
-static bool	atoll___ok(int argc, t_ecode *err_code, t_ecode *code);
-static bool	values___ok(int argc, t_param *par, t_ecode *code);
 static bool	alloc__mem(t_all *all, t_ecode *code);
 
 /**
@@ -36,8 +33,8 @@ int	main(int argc, char **argv)
 	t_all	all;
 
 	code = E_NONE;
-	if (!parse__args(argc, argv, &all.param, &code))
-		return (herr(code, "main: parse__args failed\n"));
+	if (!parse_args(argc, argv, &all.param, &code))
+		return (herr(code, "main: parse_args failed\n"));
 	if (!alloc__mem(&all, &code))
 		return (herr(code, "main: alloc__mem failed\n"));
 	if (!init_rest(&all, &code))
@@ -52,6 +49,29 @@ int	main(int argc, char **argv)
 	herr_free(E_NONE, "main: regular end", &all);
 	return (E_NONE);
 }
+
+static bool alloc__mem(t_all *all, t_ecode *code)
+{
+	long long	n;
+
+	n = all->param.num_philos;
+	all->philo = malloc(n * sizeof(t_philo));
+	all->fork = malloc(n * sizeof(t_fork));
+	all->perm.pattern = malloc(n * sizeof(bool));
+	if (all->philo == NULL || all->fork == NULL || all->perm.pattern == NULL)
+	{
+		free(all->philo);
+		free(all->fork);
+		free(all->perm.pattern);
+		all->philo = NULL;
+		all->fork = NULL;
+		all->perm.pattern = NULL;
+		*code = E_ALLOC;
+		return (false);
+	}
+	return (true);
+}
+
 
 //
 //  Worüber ich mir noch klar werden muss:
@@ -91,88 +111,3 @@ int	main(int argc, char **argv)
 //  R E M A R K S:
 //  @ (*): modelled by add  +1  to shift var ( mod ... )
 
-static bool	parse__args(int argc, char **argv, t_param *param, t_ecode *code)
-{
-	t_ecode	err_code[5];
-
-	if (argc < 1 + 4 || argc > 1 + 5)
-		return (*code = E_ARGC, false);
-	err_code[0] = E_ATOLL_NO_ERR;
-	err_code[1] = E_ATOLL_NO_ERR;
-	err_code[2] = E_ATOLL_NO_ERR;
-	err_code[3] = E_ATOLL_NO_ERR;
-	err_code[4] = E_ATOLL_NO_ERR;
-	param->num_philos = atoll_strict(argv[1], &err_code[0]);
-	param->tt_die = atoll_strict(argv[2], &err_code[1]);
-	param->tt_eat = atoll_strict(argv[3], &err_code[2]);
-	param->tt_sleep = atoll_strict(argv[4], &err_code[3]);
-	param->max_num_meals = -1;
-	if (argc == 1 + 5)
-		param->max_num_meals = atoll_strict(argv[5], &err_code[4]);
-	if (!atoll___ok(argc, err_code, code))
-		return (false);
-	if (!values___ok(argc, param, code))
-		return (false);
-	return (*code = E_NONE, true);
-}
-
-static bool	atoll___ok(int argc, t_ecode *err_code, t_ecode *code)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < 4)
-	{
-		if (err_code[i] != E_ATOLL_NO_ERR)
-			return (*code = err_code[i], false);
-		i++;
-	}
-	if (argc == 1 + 5 && err_code[4] != E_ATOLL_NO_ERR)
-		return (*code = err_code[4], false);
-	return (*code = E_NONE, true);
-}
-
-static bool	values___ok(int argc, t_param *par, t_ecode *code)
-{
-	if (par->num_philos < 1)
-		return (*code = E_NUM_PHILOS_NOT_POS, false);
-	if (par->tt_die < 1)
-		return (*code = E_TIME_TO_DIE_NOT_POS, false);
-	if (par->tt_eat < 1)
-		return (*code = E_TIME_TO_EAT_NOT_POS, false);
-	if (par->tt_sleep < 0)
-		return (*code = E_TIME_TO_SLEEP_NEG, false);
-	if (argc == 1 + 5 && par->max_num_meals < 0)
-		return (*code = E_MAX_NUM_MEALS_NEG, false);
-	if (par->num_philos > MAX_NUM_PHILOS
-		|| par->tt_die > MAX_TT_DIE
-		|| par->tt_eat > MAX_TT_EAT
-		|| par->tt_sleep > MAX_TT_SLEEP
-		|| (argc == 1 + 5 && par->max_num_meals > MAX_NUM_MEALS)
-		|| (par->tt_eat > ONE_HOUR_IN_MS && par->tt_die > ONE_HOUR_IN_MS)
-		|| (par->tt_sleep > ONE_HOUR_IN_MS && par->tt_die > ONE_HOUR_IN_MS))
-		return (*code = E_DISRESPECT, false);
-	return (*code = E_NONE, true);
-}
-
-static bool alloc__mem(t_all *all, t_ecode *code)
-{
-	long long	n;
-
-	n = all->param.num_philos;
-	all->philo = malloc(n * sizeof(t_philo));
-	all->fork = malloc(n * sizeof(t_fork));
-	all->perm.pattern = malloc(n * sizeof(bool));
-	if (all->philo == NULL || all->fork == NULL || all->perm.pattern == NULL)
-	{
-		free(all->philo);
-		free(all->fork);
-		free(all->perm.pattern);
-		all->philo = NULL;
-		all->fork = NULL;
-		all->perm.pattern = NULL;
-		*code = E_ALLOC;
-		return (false);
-	}
-	return (true);
-}
