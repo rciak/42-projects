dd60e7eda3c63c78505c208e0b468c92721fea3f
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-02-08 15:59:55 +0100

    Ci3-philosophers|feat: herr_free partially reimplemented / adopted: valgrind ok
    no memory leaks

--- Ci3-philosophers/philo/src/herr_free.c
+++ Ci3-philosophers/philo/src/herr_free.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 14:51:47 by reciak            #+#    #+#             */
-/*   Updated: 2026/02/08 11:18:45 by reciak           ###   ########.fr       */
+/*   Updated: 2026/02/08 15:59:18 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@
  * @brief herr() plus additional freeing of heap allocated mem
  * @param[in] code, an error code defined in philosophers.h
  * @param[in] debug_info, a debug message (might be NULL or "")
+ * @param[in] phi, the main struct
  * @return \p code
  */
 int	herr_free(t_ecode code, const char *debug_info, t_phi *phi)
 {
-	free(phi->philo);
-	free(phi->fork);
-	free(phi->perm.pattern);
-	pthread_mutex_destroy(&phi->perm.mutex);
-	pthread_mutex_destroy(&phi->lock_philos_till_start);
-	pthread_mutex_destroy(&phi->lock_dead);
-	pthread_mutex_destroy(&phi->lock_end_simul);
-	pthread_mutex_destroy(&phi->lock_still_love_pasta);
+	free(phi->share.maestro->allows);
+	free(phi->mutab->fork);
+	// pthread_mutex_destroy(&phi->perm.mutex);
+	// pthread_mutex_destroy(&phi->lock_philos_till_start);
+	// pthread_mutex_destroy(&phi->lock_dead);
+	// pthread_mutex_destroy(&phi->lock_end_simul);
+	// pthread_mutex_destroy(&phi->lock_still_love_pasta);
 	return (herr(code, debug_info));
 }
