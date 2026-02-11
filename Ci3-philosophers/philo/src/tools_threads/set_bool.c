a4a5c4ffb7541601fd57eca0ec8c1d5e7e316a1b
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-02-11 11:15:16 +0100

    Ci3-philosophers|feat: Added mutex-protected setter functions (bool and int64)

---
+++ Ci3-philosophers/philo/src/tools_threads/set_bool.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_bool.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 10:56:38 by reciak            #+#    #+#             */
+/*   Updated: 2026/02/11 11:04:31 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file set_bool.c
+ * @brief Stores the definition of set_bool()
+ */
+
+#include "philosophers.h"
+
+/**
+ * @brief Mutex protected stetting of a bool variable (shared among threads)
+ */
+void	set_bool(bool *var, bool value, pthread_mutex_t *mutex)
+{
+	pthread_mutex_lock(mutex);
+	*var = value;
+	pthread_mutex_unlock(mutex);
+}
