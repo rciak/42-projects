7127061f256cd15574229b07830d1445dd5896ef
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-02-11 11:15:16 +0100

    Ci3-philosophers|feat: Added mutex-protected setter functions (bool and int64)

---
+++ Ci3-philosophers/philo/src/tools_threads/set_int64.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_int64.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 11:05:07 by reciak            #+#    #+#             */
+/*   Updated: 2026/02/11 11:14:11 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file set_int64.c
+ * @brief Stores the definition of set_int64()
+ */
+
+#include "philosophers.h"
+
+/**
+ * @brief Mutex protected stetting of a int64_t variable (shared among threads)
+ */
+void	set_int64(int64_t *var, int64_t value, pthread_mutex_t *mutex)
+{
+	pthread_mutex_lock(mutex);
+	*var = value;
+	pthread_mutex_unlock(mutex);
+}
