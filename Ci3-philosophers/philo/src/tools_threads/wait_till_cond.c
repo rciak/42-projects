72fea4520d5150aec83804d6af15d4543f6c2b48
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-02-13 11:08:12 +0100

    Ci3-philosophers|feat: Implemented  wait_till_cond

---
+++ Ci3-philosophers/philo/src/tools_threads/wait_till_cond.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wait_till_cond.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/13 09:55:30 by reciak            #+#    #+#             */
+/*   Updated: 2026/02/13 11:06:33 by reciak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file wait_till_cond.c
+ * @brief Stores the definition of wait_till_cond()
+ */
+
+#include "philosophers.h"
+
+/**
+ * @brief Waits till a condition is has been meet, then
+ *        resfreshes then the state for reuse
+ */
+void	wait_till_cond(bool *state, bool wanted, pthread_mutex_t *mtx, int act)
+{
+	useconds_t	duration;
+	bool		local_state;
+
+	duration = 200;
+	local_state = !wanted;
+	while (local_state != wanted)
+	{
+		pthread_mutex_lock(mtx);
+		local_state = *state;
+		pthread_mutex_unlock(mtx);
+		usleep(duration);
+	}
+	if (act == RESET_STATE)
+	{
+		pthread_mutex_lock(mtx);
+		*state = !wanted;
+		pthread_mutex_unlock(mtx);
+	}
+}
