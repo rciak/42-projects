5e01539495b712ed6936c0830217a7e99609700f
Rene Angelo Ciak <reciak@c1r7p1.42vienna.com>
2026-02-22 18:28:16 +0100

    Ci3-philosophers|style: Changed a magic number -1 by a named constant

--- Ci3-philosophers/philo/src/debug/print_parsed_args.c
+++ Ci3-philosophers/philo/src/debug/print_parsed_args.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 23:22:14 by reciak            #+#    #+#             */
-/*   Updated: 2026/02/08 11:39:50 by reciak           ###   ########.fr       */
+/*   Updated: 2026/02/22 18:20:42 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	print_parsed_args(t_param param)
 	printf("  Time to die (ms):       %ld\n", param.tt.die);
 	printf("  Time to eat (ms):       %ld\n", param.tt.eat);
 	printf("  Time to sleep (ms):     %ld\n", param.tt.sleep);
-	if (param.meals_at_least == -1)
+	if (param.meals_at_least == OMITTED_PARAM)
 		printf("  Max number of meals:    unlimited\n");
 	else
 		printf("  Max number of meals:    %ld\n", param.meals_at_least);
