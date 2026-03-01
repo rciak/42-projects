33f2e8d275d8814970441c6c10a644c99b0e060e
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2026-03-01 16:49:01 +0100

    Ci3-philosophers|chore: Tidied up folder src/debug

--- Ci3-philosophers/philo/src/debug/print_allows.c
+++ Ci3-philosophers/philo/src/debug/print_allows.c
@@ -6,22 +6,21 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 11:58:28 by reciak            #+#    #+#             */
-/*   Updated: 2026/02/20 12:09:11 by reciak           ###   ########.fr       */
+/*   Updated: 2026/03/01 16:48:12 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include <stdio.h>
-#include "debug.h"
 
-void print_allows(bool *allows, int64_t n)
+void	print_allows(bool *allows, int64_t n)
 {
 	int64_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		printf(   YELLOW"maestro: allows[-1+%li]: %i"RESET"\n", i+1, allows[i]);
+		printf(YELLOW"maestro: allows[-1+%li]: %i"RESET"\n", i + 1, allows[i]);
 		i++;
 	}
 }
