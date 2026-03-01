4ba440b01f06932766fcc90b7897dfc3128ec24e
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2026-03-01 01:03:08 +0100

    Ci3-philosophers|transport: An optimization try: More fancy  busy__wait
    (static helper of wait_till)

--- Ci3-philosophers/philo/src/tools_time/wait_till.c
+++ Ci3-philosophers/philo/src/tools_time/wait_till.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 12:35:29 by reciak            #+#    #+#             */
-/*   Updated: 2026/02/27 22:36:18 by reciak           ###   ########.fr       */
+/*   Updated: 2026/03/01 01:02:03 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,5 +55,21 @@ static void	not__so_busy_wait(int64_t t_stop, t_squad_end *s_end)
 
 static void busy__wait(int64_t t_stop, t_squad_end *s_end)
 {
-	while (gettimeofday_musec() < t_stop && !time_to_say_goodbye(s_end));
+	int64_t	t_start;
+	int64_t	delta_t;
+	int64_t	i;
+
+	t_start = gettimeofday_musec();
+	delta_t = (t_stop - t_start) / 10;                                            // Currently just a magic number; even 1 did unexpectedly not produce so bad result!???
+	i = 0;
+	while (gettimeofday_musec() < t_stop)
+	{	
+		if (gettimeofday_musec() > t_start + i * delta_t)
+		{
+			i++;
+			if (time_to_say_goodbye(s_end))
+				break;
+		}
+	}
+	//while (gettimeofday_musec() < t_stop && !time_to_say_goodbye(s_end));
 }
