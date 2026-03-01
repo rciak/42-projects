4cef09b19b33caeeb532e9dbfb0846be9fa64940
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2026-03-01 16:23:10 +0100

    Ci3-philosophers|chore: Tidied up wait_till, incl. norminetting

--- Ci3-philosophers/philo/src/tools_time/wait_till.c
+++ Ci3-philosophers/philo/src/tools_time/wait_till.c
@@ -6,7 +6,7 @@
 /*   By: reciak <reciak@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 12:35:29 by reciak            #+#    #+#             */
-/*   Updated: 2026/03/01 15:59:12 by reciak           ###   ########.fr       */
+/*   Updated: 2026/03/01 16:22:03 by reciak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,33 +42,19 @@ static void	not__so_busy_wait(int64_t t_stop, t_squad_end *s_end)
 		timestamp = gettimeofday_musec();
 		waiting_time = t_stop - timestamp;
 		if (waiting_time <= TH_BUSY_WAIT)
-			return;
+			return ;
 		else if (TH_BUSY_WAIT < waiting_time && waiting_time <= TH_SINGLE_WAIT)
 			usleep(waiting_time - TH_BUSY_WAIT);
-		else if (TH_SINGLE_WAIT < waiting_time && waiting_time <= 2 * TH_SINGLE_WAIT - TH_BUSY_WAIT)
+		else if (TH_SINGLE_WAIT < waiting_time
+			&& waiting_time <= 2 * TH_SINGLE_WAIT - TH_BUSY_WAIT)
 			usleep(TH_SINGLE_WAIT - TH_BUSY_WAIT);
 		else if (2 * TH_SINGLE_WAIT - TH_BUSY_WAIT < waiting_time)
 			usleep(TH_SINGLE_WAIT);
 	}
 }
 
-static void busy__wait(int64_t t_stop, t_squad_end *s_end)
+static void	busy__wait(int64_t t_stop, t_squad_end *s_end)
 {
-	// int64_t	t_start;
-	// int64_t	delta_t;
-	// int64_t	i;
-
-	// t_start = gettimeofday_musec();
-	// delta_t = (t_stop - t_start) / 10;                                            // Currently just a magic number; even 1 did unexpectedly not produce so bad result!???
-	// i = 0;
-	// while (gettimeofday_musec() < t_stop)
-	// {	
-	// 	if (gettimeofday_musec() > t_start + i * delta_t)
-	// 	{
-	// 		i++;
-	// 		if (time_to_say_goodbye(s_end))
-	// 			break;
-	// 	}
-	// }
-	while (gettimeofday_musec() < t_stop && !time_to_say_goodbye(s_end));
+	while (gettimeofday_musec() < t_stop && !time_to_say_goodbye(s_end))
+		;
 }
