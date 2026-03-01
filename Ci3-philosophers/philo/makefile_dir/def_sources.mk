d235966fa8167325417c3e8407b7ad5a27dc9130
Rene Angelo Ciak <reciak@c2r1p1.42vienna.com>
2026-03-01 15:53:38 +0100

    Ci3-philosophers|chore: Moved file to other subfolder

--- Ci3-philosophers/philo/makefile_dir/def_sources.mk
+++ Ci3-philosophers/philo/makefile_dir/def_sources.mk
@@ -18,11 +18,11 @@ src/tools_libft/ft_strlen.c \
 src/tools_libft/is_in.c \
 src/tools_libft/putstr_fd.c \
 src/tools_libft/skip.c \
-src/tools_misc/time_to_say_goodbye.c \
 src/tools_threads/get_bool.c \
 src/tools_threads/get_int64.c \
 src/tools_threads/set_bool.c \
 src/tools_threads/set_int64.c \
+src/tools_threads/time_to_say_goodbye.c \
 src/tools_threads/wait_till_cond.c \
 src/tools_time/gettimeofday_musec.c \
 src/tools_time/wait_till.c
